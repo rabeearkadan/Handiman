56b33d9886ec74153798cd3ca42cce4b3915081b
mohammad <itani0369-@hotmail.com>
2019-12-21 18:11:40 +0200

    api_edited

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
-Route::get('users',"API\HomeController@users");
-Auth::routes();
+
+
