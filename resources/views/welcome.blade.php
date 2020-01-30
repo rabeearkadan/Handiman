3c94a7fd0bbf251944f107e3a4f67d89011958e3
mohammad <itani0369-@hotmail.com>
2020-01-30 13:29:49 +0200

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,9 @@
     <link href="favicon.ico" rel="shortcut icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
+        rel="stylesheet">
 
     <!-- Bootstrap CSS File -->
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -59,7 +61,8 @@ Hero Section
             </div>
 
             <h1>Welcome to Imperial studios</h1>
-            <h2>We create <span class="rotating">beautiful graphics, functional websites, working mobile apps</span></h2>
+            <h2>We create <span class="rotating">beautiful graphics, functional websites, working mobile apps</span>
+            </h2>
             <div class="actions">
                 <a href="#about" class="btn-get-started">Get Strated</a>
                 <a href="#services" class="btn-services">Our Services</a>
@@ -75,7 +78,7 @@ Header Section
     <div class="container">
 
         <div id="logo" class="pull-left">
-            <a href="#hero"><img src="img/logo.png" alt="" title="" /></a>
+            <a href="#hero"><img src="img/logo.png" alt="" title=""/></a>
             <!-- Uncomment below if you prefer to use a text image -->
             <!--<h1><a href="#hero">Header 1</a></h1>-->
         </div>
@@ -122,7 +125,8 @@ About Section
             <div class="col-md-12">
                 <h3 class="section-title">About Us</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam</p>
             </div>
         </div>
     </div>
@@ -136,15 +140,21 @@ About Section
             <div class="col-md-6 about-content">
                 <h2 class="about-title">We provide great services and ideass</h2>
                 <p class="about-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor
                     in reprehenderit in voluptate
                 </p>
                 <p class="about-text">
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim
                     id est laborum
                 </p>
                 <p class="about-text">
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt molli.
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt molli.
                 </p>
             </div>
         </div>
@@ -160,7 +170,8 @@ Services Section
             <div class="col-md-12">
                 <h3 class="section-title">Our Services</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium</p>
+                <p class="section-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                    praesentium</p>
             </div>
         </div>
 
@@ -168,32 +179,38 @@ Services Section
             <div class="col-md-4 service-item">
                 <div class="service-icon"><i class="fa fa-desktop"></i></div>
                 <h4 class="service-title"><a href="">Lorem Ipsum</a></h4>
-                <p class="service-description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                <p class="service-description">Voluptatum deleniti atque corrupti quos dolores et quas molestias
+                    excepturi sint occaecati cupiditate non provident</p>
             </div>
             <div class="col-md-4 service-item">
                 <div class="service-icon"><i class="fa fa-bar-chart"></i></div>
                 <h4 class="service-title"><a href="">Dolor Sitema</a></h4>
-                <p class="service-description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                <p class="service-description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat tarad limino ata</p>
             </div>
             <div class="col-md-4 service-item">
                 <div class="service-icon"><i class="fa fa-paper-plane"></i></div>
                 <h4 class="service-title"><a href="">Sed ut perspiciatis</a></h4>
-                <p class="service-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                <p class="service-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu fugiat nulla pariatur</p>
             </div>
             <div class="col-md-4 service-item">
                 <div class="service-icon"><i class="fa fa-photo"></i></div>
                 <h4 class="service-title"><a href="">Magni Dolores</a></h4>
-                <p class="service-description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p class="service-description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum</p>
             </div>
             <div class="col-md-4 service-item">
                 <div class="service-icon"><i class="fa fa-road"></i></div>
                 <h4 class="service-title"><a href="">Nemo Enim</a></h4>
-                <p class="service-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                <p class="service-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                    praesentium voluptatum deleniti atque</p>
             </div>
             <div class="col-md-4 service-item">
                 <div class="service-icon"><i class="fa fa-shopping-bag"></i></div>
                 <h4 class="service-title"><a href="">Eiusmod Tempor</a></h4>
-                <p class="service-description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+                <p class="service-description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero
+                    tempore, cum soluta nobis est eligendi</p>
             </div>
         </div>
     </div>
@@ -207,7 +224,8 @@ Subscrbe Section
         <div class="row">
             <div class="col-md-8">
                 <h3 class="subscribe-title">Subscribe For Updates</h3>
-                <p class="subscribe-text">Join our 1000+ subscribers and get access to the latest tools, freebies, product announcements and much more!</p>
+                <p class="subscribe-text">Join our 1000+ subscribers and get access to the latest tools, freebies,
+                    product announcements and much more!</p>
             </div>
             <div class="col-md-4 subscribe-btn-container">
                 <a class="subscribe-btn" href="#">Subscribe Now</a>
@@ -225,7 +243,8 @@ Porfolio Section
             <div class="col-md-12">
                 <h3 class="section-title">Portfolio</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">Si stante, hoc natura videlicet vult, salvam esse se, quod concedimus ses haec dicturum fuisse</p>
+                <p class="section-description">Si stante, hoc natura videlicet vult, salvam esse se, quod concedimus ses
+                    haec dicturum fuisse</p>
             </div>
         </div>
 
@@ -315,7 +334,8 @@ Testimonials Section
             <div class="col-md-12">
                 <h3 class="section-title">Testimonials</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">Erdo lide, nora porodo filece, salvam esse se, quod concedimus ses haec dicturum fuisse</p>
+                <p class="section-description">Erdo lide, nora porodo filece, salvam esse se, quod concedimus ses haec
+                    dicturum fuisse</p>
             </div>
         </div>
 
@@ -329,7 +349,9 @@ Testimonials Section
             </div>
             <div class="col-md-9">
                 <div class="quote">
-                    <b><img src="img/quote_sign_left.png" alt=""></b> Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper. <small><img src="img/quote_sign_right.png" alt=""></small>
+                    <b><img src="img/quote_sign_left.png" alt=""></b> Proin iaculis purus consequat sem cure digni ssim
+                    donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et.
+                    Maecen aliquam, risus at semper. <small><img src="img/quote_sign_right.png" alt=""></small>
                 </div>
             </div>
         </div>
@@ -337,7 +359,9 @@ Testimonials Section
         <div class="row">
             <div class="col-md-9">
                 <div class="quote">
-                    <b><img src="img/quote_sign_left.png" alt=""></b> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis architecto beatae. <small><img src="img/quote_sign_right.png" alt=""></small>
+                    <b><img src="img/quote_sign_left.png" alt=""></b> Sed ut perspiciatis unde omnis iste natus error
+                    sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                    inventore veritatis architecto beatae. <small><img src="img/quote_sign_right.png" alt=""></small>
                 </div>
             </div>
             <div class="col-md-3">
@@ -361,14 +385,15 @@ Team Section
             <div class="col-md-12">
                 <h3 class="section-title">Our Team</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque</p>
             </div>
         </div>
 
         <div class="row">
             <div class="col-md-3">
                 <div class="member">
-                    <div class="pic"><img src="img/team-1.jpg" alt=""></div>
+                    <div class="pic"><img src="../../public/img/team-1.jpg" alt=""></div>
                     <h4>Walter White</h4>
                     <span>Chief Executive Officer</span>
                     <div class="social">
@@ -382,7 +407,7 @@ Team Section
 
             <div class="col-md-3">
                 <div class="member">
-                    <div class="pic"><img src="img/team-2.jpg" alt=""></div>
+                    <div class="pic"><img src="../../public/img/team-2.jpg" alt=""></div>
                     <h4>Sarah Jhinson</h4>
                     <span>Product Manager</span>
                     <div class="social">
@@ -396,7 +421,7 @@ Team Section
 
             <div class="col-md-3">
                 <div class="member">
-                    <div class="pic"><img src="img/team-3.jpg" alt=""></div>
+                    <div class="pic"><img src="../../public/img/team-3.jpg" alt=""></div>
                     <h4>William Anderson</h4>
                     <span>CTO</span>
                     <div class="social">
@@ -410,7 +435,7 @@ Team Section
 
             <div class="col-md-3">
                 <div class="member">
-                    <div class="pic"><img src="img/team-4.jpg" alt=""></div>
+                    <div class="pic"><img src="../../public/img/team-4.jpg" alt=""></div>
                     <h4>Amanda Jepson</h4>
                     <span>Accountant</span>
                     <div class="social">
@@ -435,7 +460,8 @@ Contact Section
             <div class="col-md-12">
                 <h3 class="section-title">Contact Us</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque</p>
             </div>
         </div>
 
@@ -466,22 +492,28 @@ Contact Section
                     <div id="errormessage"></div>
                     <form action="" method="post" role="form" class="contactForm">
                         <div class="form-group">
-                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name"
+                                   data-rule="minlen:4" data-msg="Please enter at least 4 chars"/>
                             <div class="validation"></div>
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email"
+                                   data-rule="email" data-msg="Please enter a valid email"/>
                             <div class="validation"></div>
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"
+                                   data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject"/>
                             <div class="validation"></div>
                         </div>
                         <div class="form-group">
-                            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                            <textarea class="form-control" name="message" rows="5" data-rule="required"
+                                      data-msg="Please write something for us" placeholder="Message"></textarea>
                             <div class="validation"></div>
                         </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
+                        <div class="text-center">
+                            <button type="submit">Send Message</button>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -518,19 +550,19 @@ Footer
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
 <!-- Required JavaScript Libraries -->
-<script src="lib/jquery/jquery.min.js"></script>
-<script src="lib/bootstrap/js/bootstrap.min.js"></script>
-<script src="lib/superfish/hoverIntent.js"></script>
-<script src="lib/superfish/superfish.min.js"></script>
-<script src="lib/morphext/morphext.min.js"></script>
-<script src="lib/wow/wow.min.js"></script>
-<script src="lib/stickyjs/sticky.js"></script>
-<script src="lib/easing/easing.js"></script>
+<script src="../../public/lib/jquery/jquery.min.js"></script>
+<script src="../../public/lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../public/lib/superfish/hoverIntent.js"></script>
+<script src="../../public/lib/superfish/superfish.min.js"></script>
+<script src="../../public/lib/morphext/morphext.min.js"></script>
+<script src="../../public/lib/wow/wow.min.js"></script>
+<script src="../../public/lib/stickyjs/sticky.js"></script>
+<script src="../../public/lib/easing/easing.js"></script>
 
 <!-- Template Specisifc Custom Javascript File -->
-<script src="js/custom.js"></script>
+<script src="../../public/js/custom.js"></script>
 
-<script src="contactform/contactform.js"></script>
+<script src="../../public/contactform/contactform.js"></script>
 
 
 </body>
