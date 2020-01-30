8454eccdf0393210205c0b2e7a8077ead0ca3858
mohammad <itani0369-@hotmail.com>
2020-01-30 17:05:35 +0200

    /

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\NotificationSenderEvent;
+use App\Listeners\SendNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        NotificationSenderEvent::class => [
+            SendNotification::class
+        ]
     ];
 
     /**
