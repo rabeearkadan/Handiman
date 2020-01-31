6d8252cf2ce4db798ee7641d62e8b39790406beb
ahmaddorra <ahmadeldorra1@gmail.com>
2020-01-31 19:24:56 +0200

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,8 +123,8 @@
 
     </div>
 @yield('script')
-    <script src="../../public/lib/jquery/jquery.min.js"></script>
-
+{{--    <script src="../../public/lib/jquery/jquery.min.js"></script>--}}
     
+    <script src="../../public/js/custom.js"></script>
 </body>
 </html>
