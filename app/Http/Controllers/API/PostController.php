6eef718fbdc5f57c07221200050c182b57262f67
mohammad <itani0369-@hotmail.com>
2020-02-02 19:16:23 +0200

    Update PostController.php

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -1,43 +1,29 @@
 <?php
-
-
 namespace App\Http\Controllers\API;
 
-
 use App\Http\Controllers\Controller;
-
-
-
-use App\Events\NotificationSenderEvent;
+use App\Models\Post;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use phpDocumentor\Reflection\Types\Integer;
 
-class PostController extends  Controller
+class PostController extends Controller
 {
 
-    public static function getID($collection)
+    public function getPostId($id)
     {
+        $post = Post::query()->find($id);
 
-        $seq = \DB::getCollection('posts')->findOneAndUpdate(
-            array('_id' => $collection),
-            array('$inc' => array('seq' => 1)),
-            array('new' => true, 'upsert' => true, 'returnDocument' => \MongoDB\Operation\FindOneAndUpdate::RETURN_DOCUMENT_AFTER)
-        );
-        return $seq->seq;
+        return response()->json(['status' => 'success', 'post' => $post]);
     }
 
-    /**
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public static function addPost(Request $request)
+    public function addPost(Request $request)
     {
         $post = new Post();
-        $post->_id = self::getID(posts);
-
         $post->post_text = ['post_text'];
+        $post->user_id = Auth::user()->_id;
+        $post->request_id = $request->get('request_id'); // it maye be null
         $post->save();
 
         return response()->json(['status' => 'success', 'post' => $post]);
