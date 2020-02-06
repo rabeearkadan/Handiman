f661f67bb6aa625611c9477f941d4c77e90a5cee
mohammad <itani0369-@hotmail.com>
2020-02-06 15:24:16 +0200

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,172 +1,99 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <head>
-        <meta charset="utf-8">
-        <title>Imperial Boootstrap Template</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="" name="keywords">
-        <meta content="" name="description">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
-        <meta property="og:title" content="">
-        <meta property="og:image" content="">
-        <meta property="og:url" content="">
-        <meta property="og:site_name" content="">
-        <meta property="og:description" content="">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:site" content="">
-        <meta name="twitter:title" content="">
-        <meta name="twitter:description" content="">
-        <meta name="twitter:image" content="">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Place your favicon.ico and apple-touch-icon.png in the template root directory -->
-        <link href="favicon.ico" rel="shortcut icon">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-        <!-- Google Fonts -->
-        <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
-            rel="stylesheet">
-
-        <!-- Bootstrap CSS File -->
-        <link href="../../public/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Libraries CSS Files -->
-        <link href="../../public/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../../public/lib/animate-css/animate.min.css" rel="stylesheet">
-
-        <link href="../../public/css/style.css" rel="stylesheet">
-    {{--    <!-- Main Stylesheet File -->--}}
-    {{--    <link href="../../public/css/style.css" rel="stylesheet">--}}
+    <!-- Styles -->
+    <link href="../../public/css/app.css" rel="stylesheet">
+{{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
 
 
-    <!-- Main Stylesheet File -->
-        <link href="../../public/css/style.css" rel="stylesheet">
 
-
-        <!-- =======================================================
-          Theme Name: Imperial
-          Theme URL: https://bootstrapmade.com/imperial-free-onepage-bootstrap-theme/
-          Author: BootstrapMade.com
-          Author URL: https://bootstrapmade.com
-        ======================================================= -->
-    </head>
 
 </head>
-<header id="header">
-    <div class="container">
-
-        <div id="logo" class="pull-left">
-            <a href="#hero"><img src="../../public/img/logo.png" alt="" title=""/></a>
-            <!-- Uncomment below if you prefer to use a text image -->
-            <!--<h1><a href="#hero">Header 1</a></h1>-->
-        </div>
-
-        <nav id="nav-menu-container">
-            <ul class="nav-menu">
-                <li class="menu-active"><a href="#hero">Home</a></li>
-                <li><a href="#about">About Us</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#testimonials">Testimonials</a></li>
-                <li><a href="#team">Team</a></li>
-                <li class="menu-has-children"><a href="">Drop Down</a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="menu-has-children"><a href="#">Drop Down 2</a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                        <li><a href="#">Drop Down 5</a></li>
-                    </ul>
-                </li>
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
-                <li><a href="{{route('admin')}}">Admin</a></li>
-            </ul>
-        </nav>
-        <!-- #nav-menu-container -->
-    </div>
-
-</header>
 <body>
-<div id="app">
-    {{--        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
-    {{--            <div class="container">--}}
-    {{--                <a class="navbar-brand" href="{{ url('/') }}">--}}
-    {{--                    {{ config('app.name', 'Laravel') }}--}}
-    {{--                </a>--}}
-    {{--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-    {{--                    <span class="navbar-toggler-icon"></span>--}}
-    {{--                </button>--}}
+    <div id="app">
+{{--        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
+{{--            <div class="container">--}}
+{{--                <a class="navbar-brand" href="{{ url('/') }}">--}}
+{{--                    {{ config('app.name', 'Laravel') }}--}}
+{{--                </a>--}}
+{{--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
+{{--                    <span class="navbar-toggler-icon"></span>--}}
+{{--                </button>--}}
 
-    {{--                <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-    {{--                    <!-- Left Side Of Navbar -->--}}
-    {{--                    <ul class="navbar-nav mr-auto">--}}
+{{--                <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--                    <!-- Left Side Of Navbar -->--}}
+{{--                    <ul class="navbar-nav mr-auto">--}}
 
-    {{--                    </ul>--}}
+{{--                    </ul>--}}
 
-    {{--                    <!-- Right Side Of Navbar -->--}}
-    {{--                    <li class="navbar-nav ml-auto">--}}
+{{--                    <!-- Right Side Of Navbar -->--}}
+{{--                    <li class="navbar-nav ml-auto">--}}
 
-    {{--                        <!-- Authentication Links -->--}}
-    {{--                        @guest--}}
-    {{--                            <li class="nav-item">--}}
-    {{--                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
-    {{--                            </li>--}}
-    {{--                            @if (Route::has('register'))--}}
-    {{--                                <li class="nav-item">--}}
-    {{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
-    {{--                                </li>--}}
-    {{--                            @endif--}}
-    {{--                        @else--}}
-
-
-    {{--                            <li class="nav-item dropdown">--}}
-    {{--                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
-    {{--                                    {{ Auth::user()->name }} <span class="caret"></span>--}}
-    {{--                                </a>--}}
-
-    {{--                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
-    {{--                                    <a class="dropdown-item" href="{{ route('logout') }}"--}}
-    {{--                                       onclick="event.preventDefault();--}}
-    {{--                                                     document.getElementById('logout-form').submit();">--}}
-    {{--                                        {{ __('Logout') }}--}}
-    {{--                                    </a>--}}
-
-    {{--                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-    {{--                                        @csrf--}}
-    {{--                                    </form>--}}
-    {{--                                </div>--}}
-    {{--                            </li>--}}
-    {{--                        @endguest--}}
-    {{--                    </ul>--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-    {{--        </nav>--}}
+{{--                        <!-- Authentication Links -->--}}
+{{--                        @guest--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+{{--                            </li>--}}
+{{--                            @if (Route::has('register'))--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                                </li>--}}
+{{--                            @endif--}}
+{{--                        @else--}}
 
 
-    {{--        <header id="header">--}}
-    {{--            <nav id="nav-menu-container">--}}
-    {{--                <ul class="nav-menu">--}}
-    {{--                    @yield('navContent')--}}
-    {{--                </ul>--}}
-    {{--            </nav>--}}
-    {{--        </header>--}}
+{{--                            <li class="nav-item dropdown">--}}
+{{--                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                                    {{ Auth::user()->name }} <span class="caret"></span>--}}
+{{--                                </a>--}}
+
+{{--                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                                    <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                                       onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">--}}
+{{--                                        {{ __('Logout') }}--}}
+{{--                                    </a>--}}
+
+{{--                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+{{--                                        @csrf--}}
+{{--                                    </form>--}}
+{{--                                </div>--}}
+{{--                            </li>--}}
+{{--                        @endguest--}}
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </nav>--}}
 
 
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
+{{--        <header id="header">--}}
+{{--            <nav id="nav-menu-container">--}}
+{{--                <ul class="nav-menu">--}}
+{{--                    @yield('navContent')--}}
+{{--                </ul>--}}
+{{--            </nav>--}}
+{{--        </header>--}}
+
+
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
