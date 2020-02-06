5574206e250d8a2eda16423666813c121b843168
mohammad <itani0369-@hotmail.com>
2020-02-06 15:34:43 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
 
 
 
-@push('css')
+
     <!-- Google Fonts -->
         <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
@@ -47,7 +47,6 @@
 
     <!-- Main Stylesheet File -->
         <link href="../../public/css/style.css" rel="stylesheet">
-@endpush
 
 <!-- =======================================================
       Theme Name: Imperial
@@ -58,7 +57,7 @@
 </head>
 
 <body>
-
+<div id="preloader"></div>
 
 <section id="hero">
     <div class="hero-container">
@@ -556,7 +555,7 @@ Footer
 <!-- #footer -->
 
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-@push('script')
+
     <!-- Required JavaScript Libraries -->
     <script src="../../public/lib/jquery/jquery.min.js"></script>
     <script src="../../public/lib/bootstrap/js/bootstrap.min.js"></script>
@@ -571,7 +570,7 @@ Footer
     <script src="../../public/js/custom.js"></script>
 
     <script src="../../public/contactform/contactform.js"></script>
-@endpush
+
 
 </body>
 
