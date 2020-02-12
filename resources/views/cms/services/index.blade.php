833dc45da9935019e73b672520a2536d1f273d4b
mohammad <itani0369-@hotmail.com>
2020-02-12 23:53:28 +0200

    Update index.blade.php

--- resources/views/cms/services/index.blade.php
+++ resources/views/cms/services/index.blade.php
@@ -5,6 +5,7 @@
         <div class="col-lg-12">
             <div class="main-card mb-3 card">
                 <div class="card-body"><h5 class="card-title">Service Table</h5>
+                    <button class="mb-2 mr-2 btn btn-primary" onclick="location.href='{{route('service.create')}}'">Add New Service</button>
                     <table class="mb-0 table">
                         <thead>
                         <tr>
@@ -20,7 +21,12 @@
                                <th scope="row">{{$loop->index +1 }}</th>
                                <td>{{$service->name}}</td>
                                <td>{{$service->users()->count()}}</td>
-                               <td>action to be added</td>
+                               <td>
+                                   <a href="{{route('service.edit', $service->id)}}"><i class="pe-7s-pen"> </i></a>
+                                   <a href="{{route('service.show', $service->id)}}"><i class="pe-7s-display"> </i></a>
+                                   // modal as popup delete to be added
+                                   <i class="pe-7s-trash"> </i>
+                               </td>
                            </tr>
                        @endforeach
 
