322f26d47d9980505e7dc410e60febe6dcf44f0d
ahmaddorra <ahmadeldorra1@gmail.com>
2020-02-17 22:47:31 +0200

    no message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -57,6 +57,7 @@ class LoginController extends Controller
      */
     public function getRedirectTo(): string
     {
+        dd("here");
         if ( auth()->user()->role == 'admin' ){
             return  route('admin.home');
         }elseif ( auth()->user()->role == 'user_employee' || auth()->user()->role == 'employee'  ){
