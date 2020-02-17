5b629f197d76d64213dc8eeb3f2a9b1c9e6e9780
ahmaddorra <ahmadeldorra1@gmail.com>
2020-02-17 23:29:24 +0200

    no message

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Auth::routes();
 
 Route::get("contact-us", 'FRONT\HomeController@contact');
-
-
-
 Route::get("register", 'Auth\RegisterController@showRegistrationForm')->name('register');
+
+Auth::routes();
 
 //Route::get("home","FRONT\HomeController@getHomePage");
 
