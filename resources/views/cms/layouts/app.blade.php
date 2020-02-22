b28a447957d4c5f934c0dad3afd07dafa73b6117
mohammad <itani0369-@hotmail.com>
2020-02-23 00:26:18 +0200

    Update app.blade.php

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -32,9 +32,9 @@
         </div>
 
     </div>
-
-    <script type="text/javascript" src="{{asset('scripts/main.js')}}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="{{asset('scripts/main.js')}}"></script>
+
 
 
 
