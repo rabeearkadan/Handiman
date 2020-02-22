094e8839846406faeda5648e8f538b77a9c40594
mohammad <itani0369-@hotmail.com>
2020-02-23 00:24:31 +0200

    Update app.blade.php

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -30,8 +30,14 @@
             </div>
             @include('cms.layouts.footer')
         </div>
-        <script type="text/javascript" src="{{asset('scripts/main.js')}}"></script>
+
     </div>
+
+    <script type="text/javascript" src="{{asset('scripts/main.js')}}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+
+
 @stack('js')
 
 
