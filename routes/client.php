1cdda3afb413c63788f848ac333b13bdb21e01bc
ahmaddorra <ahmadeldorra1@gmail.com>
2020-02-22 19:39:51 +0200

    no message

--- routes/client.php
+++ routes/client.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home','HomeController@index')->name('client.home');
 Route::get('/services/{id?}','HomeController@service')->name('client.service');
-Route::get('/{user_id?}','ProfileController@userProfile')->name('client.user-profile');
+Route::get('/{user_id}','ProfileController@userProfile')->name('client.user-profile');
