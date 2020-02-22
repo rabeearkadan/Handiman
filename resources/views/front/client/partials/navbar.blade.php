a479cb6e041c928ef15f00e10e65123ebe925370
ahmaddorra <ahmadeldorra1@gmail.com>
2020-02-22 18:26:11 +0200

    no message

--- resources/views/front/client/partials/navbar.blade.php
+++ resources/views/front/client/partials/navbar.blade.php
@@ -5,3 +5,29 @@
     <a href="#" class="nav-item" active-color="red">Profile</a>
     <span class="nav-indicator"></span>
 </nav>
+
+
+@push('js')
+    <script>
+        const indicator = document.querySelector('.nav-indicator');
+        const items = document.querySelectorAll('.nav-item');
+        function handleIndicator(el) {
+            items.forEach(item => {
+                item.classList.remove('is-active');
+                item.removeAttribute('style');
+            });
+
+            indicator.style.width = `${el.offsetWidth}px`;
+            indicator.style.left = `${el.offsetLeft}px`;
+            indicator.style.backgroundColor = el.getAttribute('active-color');
+
+            el.classList.add('is-active');
+            el.style.color = el.getAttribute('active-color');
+        }
+        items.forEach((item, index) => {
+            item.addEventListener('click', (e) => { handleIndicator(e.target)});
+            item.classList.contains('is-active') && handleIndicator(item);
+        });
+    </script>
+
+@endpush
--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -22,36 +22,19 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 <script>
     $(document).ready(function($) {
         var Body = $('body');
         Body.addClass('preloader-site');
     });
-    $(window).on('load',function() {
+    setInterval(function(){
         $('.preloader-wrapper').fadeOut();
         $('body').removeClass('preloader-site');
-    });
+    },3000);
 </script>
-<script>
-    const indicator = document.querySelector('.nav-indicator');
-    const items = document.querySelectorAll('.nav-item');
-    function handleIndicator(el) {
-        items.forEach(item => {
-            item.classList.remove('is-active');
-            item.removeAttribute('style');
-        });
 
-        indicator.style.width = `${el.offsetWidth}px`;
-        indicator.style.left = `${el.offsetLeft}px`;
-        indicator.style.backgroundColor = el.getAttribute('active-color');
-
-        el.classList.add('is-active');
-        el.style.color = el.getAttribute('active-color');
-    }
-    items.forEach((item, index) => {
-        item.addEventListener('click', (e) => { handleIndicator(e.target)});
-        item.classList.contains('is-active') && handleIndicator(item);
-    });
-</script>
+@stack('js')
 </body>
 </html>
