50a5c31f6d6761cc983f828d41731d22d0a8cc81
mohammad <itani0369-@hotmail.com>
2020-02-23 14:48:40 +0200

    modal pop up

--- resources/views/cms/services/index.blade.php
+++ resources/views/cms/services/index.blade.php
@@ -26,7 +26,10 @@
 {{--                                   <a href="{{route('service.edit', $service->id)}}"><i class="pe-7s-pen"> </i></a>--}}
 {{--                                   <a href="{{route('service.show', $service->id)}}"><i class="pe-7s-display"> </i></a>--}}
 {{--                                   // modal as popup delete to be added--}}
-                                   <a  href="javascript:deleteService({{$service->id}})"><i class="pe-7s-trash"> </i></a>
+{{--                                   <a  href="javascript:deleteService({{$service->id}})"><i class="pe-7s-trash"> </i></a>--}}
+                               <button type="button" class="btn mr-2 mb-2 btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                  Delete
+                               </button>
                                </td>
                            </tr>
                        @endforeach
@@ -37,7 +40,25 @@
             </div>
         </div>
     </div>
-
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
   @endsection
 @push('js')
 
