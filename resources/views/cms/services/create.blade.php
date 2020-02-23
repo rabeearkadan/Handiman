672cf92a141e640fe5258492ea50fee8c29d8944
mohammad <itani0369-@hotmail.com>
2020-02-23 14:14:45 +0200

    Update create.blade.php

--- resources/views/cms/services/create.blade.php
+++ resources/views/cms/services/create.blade.php
@@ -24,7 +24,7 @@
                     <form action="{{route('service.store')}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="container">
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="position-relative form-group"><label for="exampleEmail11"
                                                                                  class="">Service Name</label><input
                                         name="service_name" placeholder="Example.." type="text"
