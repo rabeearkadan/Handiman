8b9451ebe6306ec2fc82d6fa8b38ae24eacd4d01
ahmaddorra <ahmadeldorra1@gmail.com>
2020-02-23 20:41:50 +0200

    no message

--- resources/views/front/client/service-users.blade.php
+++ resources/views/front/client/service-users.blade.php
@@ -11,7 +11,7 @@
         <div class = "row">
             @endif
             <div class="column">
-                <a href="{{route('client.user-profile',[$service->id,$user->id])}}"> <div class="card"> <img src="" > <label> {{$user->name}} </label> </div> </a>
+                <a href="{{route('client.user-profile',[$service->id,$user->id])}}"> <div class="card"> <img src="{{'/storage/app/public/'.$user->image}}" > <label> {{$user->name}} </label> </div> </a>
             </div>
 
             @endforeach
