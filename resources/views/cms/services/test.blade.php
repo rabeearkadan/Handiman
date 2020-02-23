48114a852ded7fba495145f8ff59c6c2a2eb55f7
mohammad <itani0369-@hotmail.com>
2020-02-23 19:51:45 +0200

    Update test.blade.php

--- resources/views/cms/services/test.blade.php
+++ resources/views/cms/services/test.blade.php
@@ -7,7 +7,7 @@
 
 
     <div class="container">
-        
+
         <table class="table table-bordered data-table">
             <thead>
             <tr>
@@ -49,6 +49,7 @@
                 columns: [
 
                     {data: 'name', name: 'name'},
+                    { data:'{{$service->users()->count()}}' ,name:'{{$service->users()->count()}}'},
                         {{--{data: '{{$service->users()->count()}}' , name:' number of employees enrolled '},--}}
                     {data: 'action', name: 'action', orderable: false, searchable: false},
                 ]
