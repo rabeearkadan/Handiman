bed811db6c2d61ca28b5fcc219d44ce2ee2aff50
mohammad <itani0369-@hotmail.com>
2020-02-23 14:52:36 +0200

    Update index.blade.php

--- resources/views/cms/services/index.blade.php
+++ resources/views/cms/services/index.blade.php
@@ -6,7 +6,9 @@
             <div class="main-card mb-3 card">
                 <div class="card-body"><h5 class="card-title">Service Table</h5>
 
-                    <button class="mb-2 mr-2 btn btn-primary" onclick="location.href='{{route('service.create')}}'">Add New Service</button>
+                    <button class="mb-2 mr-2 btn btn-primary" onclick="location.href='{{route('service.create')}}'">Add
+                        New Service
+                    </button>
                     <table class="mb-0 table">
                         <thead>
                         <tr>
@@ -17,22 +19,23 @@
                         </tr>
                         </thead>
                         <tbody>
-                       @foreach($services as $service)
-                           <tr id="row-{{$service->id}}" >
-                               <th scope="row">{{$loop->index +1 }}</th>
-                               <td>{{$service->name}}</td>
-                               <td>{{$service->users()->count()}}</td>
-                           <td>
-{{--                                   <a href="{{route('service.edit', $service->id)}}"><i class="pe-7s-pen"> </i></a>--}}
-{{--                                   <a href="{{route('service.show', $service->id)}}"><i class="pe-7s-display"> </i></a>--}}
-{{--                                   // modal as popup delete to be added--}}
-{{--                                   <a  href="javascript:deleteService({{$service->id}})"><i class="pe-7s-trash"> </i></a>--}}
-                               <button type="button" class="btn mr-2 mb-2 btn-danger" data-toggle="modal" data-target="#exampleModal">
-                                  Delete
-                               </button>
-                               </td>
-                           </tr>
-                       @endforeach
+                        @foreach($services as $service)
+                            <tr id="row-{{$service->id}}">
+                                <th scope="row">{{$loop->index +1 }}</th>
+                                <td>{{$service->name}}</td>
+                                <td>{{$service->users()->count()}}</td>
+                                <td>
+                                    {{--                                   <a href="{{route('service.edit', $service->id)}}"><i class="pe-7s-pen"> </i></a>--}}
+                                    {{--                                   <a href="{{route('service.show', $service->id)}}"><i class="pe-7s-display"> </i></a>--}}
+                                    {{--                                   // modal as popup delete to be added--}}
+                                    {{--                                   <a  href="javascript:deleteService({{$service->id}})"><i class="pe-7s-trash"> </i></a>--}}
+                                    <button type="button" class="btn mr-2 mb-2 btn-danger" data-toggle="modal"
+                                            data-target="#exampleModal">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
 
                         </tbody>
                     </table>
@@ -41,7 +44,7 @@
         </div>
     </div>
 
-  @endsection
+@endsection
 @push('js')
 
     <script type="text/javascript">
@@ -65,22 +68,23 @@
             });
         }
     </script>
-    @endpush
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+@endpush
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Delete Service</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+                <p class="mb-0">Are you sure you want to delete this service?</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-danger">Delete and Save Changes</button>
             </div>
         </div>
     </div>
