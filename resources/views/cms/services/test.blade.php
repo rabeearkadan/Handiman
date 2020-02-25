85be3a673f53161370241f3945d38c03cec30352
mohammad <itani0369-@hotmail.com>
2020-02-25 22:40:14 +0200

    Update test.blade.php

--- resources/views/cms/services/test.blade.php
+++ resources/views/cms/services/test.blade.php
@@ -77,75 +77,76 @@
             <br/>
             <div class="table-responsive">
                 <table id="user_table" class="table table-bordered table-striped">
-                    <thead>
-                    <tr>
-                        <th width="20%">Service Name</th>
-                        <th width="20%"> #</th>
-                        <th width="20"></th>
-                        <th width="30%">Action</th>
-                    </tr>
-                    </thead>
+{{--                    <thead>--}}
+{{--                    <tr>--}}
+{{--                        <th width="20%">Service Name</th>--}}
+{{--                        <th width="20%"> #</th>--}}
+{{--                        <th width="20"></th>--}}
+{{--                        <th width="30%">Action</th>--}}
+{{--                    </tr>--}}
+{{--                    </thead>--}}
                 </table>
             </div>
 
         </div>
     </div>
+    <div id="formModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Add New Record</h4>
+                </div>
+                <div class="modal-body">
+                    <span id="form_result"></span>
+                    <form method="post" id="sample_form" class="form-horizontal">
+                        @csrf
+                        <div class="form-group">
+                            <label class="control-label col-md-4">First Name : </label>
+                            <div class="col-md-8">
+                                <input type="text" name="first_name" id="first_name" class="form-control"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-4">Last Name : </label>
+                            <div class="col-md-8">
+                                <input type="text" name="last_name" id="last_name" class="form-control"/>
+                            </div>
+                        </div>
+                        <br/>
+                        <div class="form-group" align="center">
+                            <input type="hidden" name="action" id="action" value="Add"/>
+                            <input type="hidden" name="hidden_id" id="hidden_id"/>
+                            <input type="submit" name="action_button" id="action_button" class="btn btn-warning"
+                                   value="Add"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="confirmModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h2 class="modal-title">Confirmation</h2>
+                </div>
+                <div class="modal-body">
+                    <h4 align="center" style="margin:0;">Are you sure you want to remove this data?</h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 
-<div id="formModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Add New Record</h4>
-            </div>
-            <div class="modal-body">
-                <span id="form_result"></span>
-                <form method="post" id="sample_form" class="form-horizontal">
-                    @csrf
-                    <div class="form-group">
-                        <label class="control-label col-md-4">First Name : </label>
-                        <div class="col-md-8">
-                            <input type="text" name="first_name" id="first_name" class="form-control"/>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-md-4">Last Name : </label>
-                        <div class="col-md-8">
-                            <input type="text" name="last_name" id="last_name" class="form-control"/>
-                        </div>
-                    </div>
-                    <br/>
-                    <div class="form-group" align="center">
-                        <input type="hidden" name="action" id="action" value="Add"/>
-                        <input type="hidden" name="hidden_id" id="hidden_id"/>
-                        <input type="submit" name="action_button" id="action_button" class="btn btn-warning"
-                               value="Add"/>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div id="confirmModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h2 class="modal-title">Confirmation</h2>
-            </div>
-            <div class="modal-body">
-                <h4 align="center" style="margin:0;">Are you sure you want to remove this data?</h4>
-            </div>
-            <div class="modal-footer">
-                <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 @push('js')
 
