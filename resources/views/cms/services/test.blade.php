2453cdbe450997d99f95fa679a7461629ab2ed9b
mohammad <itani0369-@hotmail.com>
2020-02-25 23:59:49 +0200

    .

--- resources/views/cms/services/test.blade.php
+++ resources/views/cms/services/test.blade.php
@@ -282,7 +282,7 @@
 @push('css')
 
     <meta name="csrf-token" content="{{ @csrf_token()}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"/>
+{{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"/>--}}
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 @endpush
