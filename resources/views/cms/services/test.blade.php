0ae52d53fa864e5346d9cb62e68372d931a10632
mohammad <itani0369-@hotmail.com>
2020-02-25 20:22:22 +0200

    ,

--- resources/views/cms/services/test.blade.php
+++ resources/views/cms/services/test.blade.php
@@ -3,9 +3,6 @@
 {{--@section('content')--}}
 
 
-
-
-
 {{--    <div class="container">--}}
 
 {{--        <table class="table table-bordered data-table">--}}
@@ -22,8 +19,6 @@
 {{--        </table>--}}
 {{--    </div>--}}
 {{--@endsection--}}
-
-
 
 
 {{--@push('js')--}}
@@ -68,164 +63,32 @@
 @section('content')
 
 
-<div
-    class="tab-content">
-<div class="container">
-    <br />
-    <h3 align="center">How to Delete or Remove Data From Mysql in Laravel 6 using Ajax</h3>
-    <br />
-    <div align="right">
-        <button type="button" name="create_record" id="create_record" class="btn btn-success btn-sm">Create Record</button>
+    <div
+        class="tab-content">
+        <div class="container">
+            <br/>
+
+            <br/>
+            <div align="right">
+                <button type="button" name="create_record" id="create_record" class="btn btn-success btn-sm">Create
+                    Record
+                </button>
+            </div>
+            <br/>
+            <div class="table-responsive">
+                <table id="user_table" class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th width="35%">Service Name</th>
+                        <th width="35%"> #</th>
+                        <th width="30%">Action</th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
+
+        </div>
     </div>
-    <br />
-    <div class="table-responsive">
-        <table id="user_table" class="table table-bordered table-striped">
-            <thead>
-            <tr>
-                <th width="35%">First Name</th>
-                <th width="35%">Last Name</th>
-                <th width="30%">Action</th>
-            </tr>
-            </thead>
-        </table>
-    </div>
-
-</div>
-</div>
-
-@push('js')
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-    <script>
-        $(document).ready(function(){
-
-            $('#user_table').DataTable({
-                processing: true,
-                serverSide: true,
-                ajax: {
-                    url: "{{ route('service.index') }}",
-                },
-                columns: [
-                    {
-                        data: 'name',
-                        name: 'name'
-                    },
-
-                    {
-                        data: 'action',
-                        name: 'action',
-                        orderable: false
-                    }
-                ]
-            });
-
-            $('#create_record').click(function(){
-                $('.modal-title').text('Add New Record');
-                $('#action_button').val('Add');
-                $('#action').val('Add');
-                $('#form_result').html('');
-
-                $('#formModal').modal('show');
-            });
-
-            $('#sample_form').on('submit', function(event){
-                event.preventDefault();
-                var action_url = '';
-
-                if($('#action').val() == 'Add')
-                {
-                    action_url = "{{ route('service.store') }}";
-                }
-
-                if($('#action').val() == 'Edit')
-                {
-                    action_url = "{{ route('service.update') }}";
-                }
-
-                $.ajax({
-                    url: action_url,
-                    method:"POST",
-                    data:$(this).serialize(),
-                    dataType:"json",
-                    success:function(data)
-                    {
-                        var html = '';
-                        if(data.errors)
-                        {
-                            html = '<div class="alert alert-danger">';
-                            for(var count = 0; count < data.errors.length; count++)
-                            {
-                                html += '<p>' + data.errors[count] + '</p>';
-                            }
-                            html += '</div>';
-                        }
-                        if(data.success)
-                        {
-                            html = '<div class="alert alert-success">' + data.success + '</div>';
-                            $('#sample_form')[0].reset();
-                            $('#user_table').DataTable().ajax.reload();
-                        }
-                        $('#form_result').html(html);
-                    }
-                });
-            });
-
-            $(document).on('click', '.edit', function(){
-                var id = $(this).attr('id');
-                $('#form_result').html('');
-                $.ajax({
-                    url :"/service/"+id+"/edit",
-                    dataType:"json",
-                    success:function(data)
-                    {
-                        $('#first_name').val(data.result.first_name);
-                        $('#last_name').val(data.result.last_name);
-                        $('#hidden_id').val(id);
-                        $('.modal-title').text('Edit Record');
-                        $('#action_button').val('Edit');
-                        $('#action').val('Edit');
-                        $('#formModal').modal('show');
-                    }
-                })
-            });
-
-            var user_id;
-
-            $(document).on('click', '.delete', function(){
-                user_id = $(this).attr('id');
-                $('#confirmModal').modal('show');
-            });
-
-            $('#ok_button').click(function(){
-                $.ajax({
-                    url:"service/destroy/"+user_id,
-                    beforeSend:function(){
-                        $('#ok_button').text('Deleting...');
-                    },
-                    success:function(data)
-                    {
-                        setTimeout(function(){
-                            $('#confirmModal').modal('hide');
-                            $('#user_table').DataTable().ajax.reload();
-                            alert('Data Deleted');
-                        }, 2000);
-                    }
-                })
-            });
-
-        });
-    </script>
-@endpush
-
-@push('css')
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
-@endpush
 @endsection
 
 
@@ -241,22 +104,23 @@
                 <form method="post" id="sample_form" class="form-horizontal">
                     @csrf
                     <div class="form-group">
-                        <label class="control-label col-md-4" >First Name : </label>
+                        <label class="control-label col-md-4">First Name : </label>
                         <div class="col-md-8">
-                            <input type="text" name="first_name" id="first_name" class="form-control" />
+                            <input type="text" name="first_name" id="first_name" class="form-control"/>
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-md-4">Last Name : </label>
                         <div class="col-md-8">
-                            <input type="text" name="last_name" id="last_name" class="form-control" />
+                            <input type="text" name="last_name" id="last_name" class="form-control"/>
                         </div>
                     </div>
-                    <br />
+                    <br/>
                     <div class="form-group" align="center">
-                        <input type="hidden" name="action" id="action" value="Add" />
-                        <input type="hidden" name="hidden_id" id="hidden_id" />
-                        <input type="submit" name="action_button" id="action_button" class="btn btn-warning" value="Add" />
+                        <input type="hidden" name="action" id="action" value="Add"/>
+                        <input type="hidden" name="hidden_id" id="hidden_id"/>
+                        <input type="submit" name="action_button" id="action_button" class="btn btn-warning"
+                               value="Add"/>
                     </div>
                 </form>
             </div>
@@ -282,3 +146,133 @@
     </div>
 </div>
 
+@push('js')
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+
+            $('#user_table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ route('service.index') }}",
+                },
+                columns: [
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+
+                    {
+                        data: 'users', name: '#users', render: function (data) {
+                            return data.length;
+                        }
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false
+                    }
+                ]
+            });
+
+            $('#create_record').click(function () {
+                $('.modal-title').text('Add New Record');
+                $('#action_button').val('Add');
+                $('#action').val('Add');
+                $('#form_result').html('');
+
+                $('#formModal').modal('show');
+            });
+
+            $('#sample_form').on('submit', function (event) {
+                event.preventDefault();
+                var action_url = '';
+
+                if ($('#action').val() == 'Add') {
+                    action_url = "{{ route('service.store') }}";
+                }
+
+                if ($('#action').val() == 'Edit') {
+                    action_url = "{{ route('service.update') }}";
+                }
+
+                $.ajax({
+                    url: action_url,
+                    method: "POST",
+                    data: $(this).serialize(),
+                    dataType: "json",
+                    success: function (data) {
+                        var html = '';
+                        if (data.errors) {
+                            html = '<div class="alert alert-danger">';
+                            for (var count = 0; count < data.errors.length; count++) {
+                                html += '<p>' + data.errors[count] + '</p>';
+                            }
+                            html += '</div>';
+                        }
+                        if (data.success) {
+                            html = '<div class="alert alert-success">' + data.success + '</div>';
+                            $('#sample_form')[0].reset();
+                            $('#user_table').DataTable().ajax.reload();
+                        }
+                        $('#form_result').html(html);
+                    }
+                });
+            });
+
+            $(document).on('click', '.edit', function () {
+                var id = $(this).attr('id');
+                $('#form_result').html('');
+                $.ajax({
+                    url: "/service/" + id + "/edit",
+                    dataType: "json",
+                    success: function (data) {
+                        $('#first_name').val(data.result.first_name);
+                        $('#last_name').val(data.result.last_name);
+                        $('#hidden_id').val(id);
+                        $('.modal-title').text('Edit Record');
+                        $('#action_button').val('Edit');
+                        $('#action').val('Edit');
+                        $('#formModal').modal('show');
+                    }
+                })
+            });
+
+            var user_id;
+
+            $(document).on('click', '.delete', function () {
+                user_id = $(this).attr('id');
+                $('#confirmModal').modal('show');
+            });
+
+            $('#ok_button').click(function () {
+                $.ajax({
+                    url: "service/destroy/" + user_id,
+                    beforeSend: function () {
+                        $('#ok_button').text('Deleting...');
+                    },
+                    success: function (data) {
+                        setTimeout(function () {
+                            $('#confirmModal').modal('hide');
+                            $('#user_table').DataTable().ajax.reload();
+                            alert('Data Deleted');
+                        }, 2000);
+                    }
+                })
+            });
+
+        });
+    </script>
+@endpush
+
+@push('css')
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css"/>
+@endpush
