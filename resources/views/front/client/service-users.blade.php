d905cbd814299b016d654b9bca4a7ae97ee5aa85
ahmaddorra <ahmadeldorra1@gmail.com>
2020-02-29 22:30:32 +0200

    no message

--- resources/views/front/client/service-users.blade.php
+++ resources/views/front/client/service-users.blade.php
@@ -3,19 +3,30 @@
     <link href="{{asset('css/client/cards.css')}}" rel="stylesheet">
 @endpush
 @section('content')
-    <div class="container" id="services">
+    <div class="container mt-2" id="services">
         <div class="row">
             @foreach($service->users as $user)
-                @if ( $loop->index  % 4 == 0 )
+                @if ( $loop->index % 4 == 0 )
         </div>
-        <div class = "row">
+        <div class="row">
             @endif
-            <div class="column">
-                <a href="{{route('client.user-profile',[$service->id,$user->id])}}"> <div class="card"> <img src="{{config('image.path').$user->image}}" > <label> {{$user->name}} </label> </div> </a>
+            <div class="col-md-3 col-sm-6">
+                <div class="card card-block">
+                    <h4 class="card-title text-right"><i class="material-icons">settings</i></h4>
+                    <a href="{{route('client.user-profile',[$service->id,$user->id])}}">
+                        <img src="{{config('image.path').$user->image}}" alt="later">
+                    </a>
+                    <h5 class="card-title  mt-3 mb-3"> {{ $user->name }} </h5>
+                    <p class="card-text">Somethinggggggg</p>
+                </div>
             </div>
-
             @endforeach
         </div>
-
     </div>
 @endsection
+
+
+
+
+
+
