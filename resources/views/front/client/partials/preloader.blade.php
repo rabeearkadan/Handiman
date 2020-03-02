de60a8dcbcf26c154f0396ed9c592f1402d73450
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-02 19:13:49 +0200

    no message

--- resources/views/front/client/partials/preloader.blade.php
+++ resources/views/front/client/partials/preloader.blade.php
@@ -1,15 +1,5 @@
 <div class="preloader-wrapper">
     <div class="preloader">
-        <section class="wrapper">
-            <div class="spinner">
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-            </div>
-        </section>
+        <div id="loading"></div>
     </div>
 </div>
