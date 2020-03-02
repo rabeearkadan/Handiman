e6a75f434325249affcdbefb832b565d572581b6
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-02 20:48:00 +0200

    no message

--- resources/views/front/client/profile/password.blade.php
+++ resources/views/front/client/profile/password.blade.php
@@ -1,7 +1,27 @@
 @extends('front.client.profile.my-profile')
 @section('profile-content')
-    <p>
-        password
-    </p>
+    <div class="content">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+                <div class="page-title">
+                    <h1>Change Password</h1>
+                </div><!-- /.page-title -->
+                <form method="post" action="?">
+                    <div class="form-group">
+                        <label for="login-form-password">Password</label>
+                        <input type="password" class="form-control" name="password" id="login-form-password">
+                    </div><!-- /.form-group -->
+
+                    <div class="form-group">
+                        <label for="login-form-password-retype">Retype password</label>
+                        <input type="password" class="form-control" name="password" id="login-form-password-retype">
+                    </div><!-- /.form-group -->
+
+                    <button type="submit" class="btn btn-primary pull-right">Change</button>
+                </form>
+            </div><!-- /.col-sm-4 -->
+        </div><!-- /.row -->
+
+    </div>
 @endsection
 
