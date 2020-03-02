3176f56e0969665724b554609fb0842693200cc1
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-02 16:20:22 +0200

    no message

--- routes/client.php
+++ routes/client.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home','HomeController@index')->name('client.home');
 Route::get('/services/{id?}','HomeController@service')->name('client.service');
 Route::get('/services/{id}/user/{user_id}','ProfileController@userProfile')->name('client.user-profile');
-Route::get('/profile','ProfileController@myProfile')->name('client.edit.profile');
-Route::get('/profile/password','ProfileController@editPassword')->name('client.edit.password');
-Route::get('/profile/payment','ProfileController@editPayment')->name('client.edit.payment');
+Route::get('/profile','ProfileController@myProfile')->name('client.profile');
+Route::get('/profile/password','ProfileController@editPassword')->name('client.password');
+Route::get('/profile/payment','ProfileController@editPayment')->name('client.payment');
 
 Route::put('/profile','ProfileController@editProfile')->name('client.edit.profile');
