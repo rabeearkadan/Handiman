5864fa65a1e294ffd222a30e37ca059f5cb9dff7
mohammad <itani0369-@hotmail.com>
2020-03-02 23:52:26 +0200

    fcm

--- config/fcm.php
+++ config/fcm.php
@@ -5,8 +5,8 @@ return [
     'log_enabled' => false,
 
     'http' => [
-        'server_key' => 'AAAAoMpgz7g:APA91bF03zGkEHDAwMlxqNuDpFE-O46v46HLsuoTfPV9g6teSA16CHzX59m5Frt2lKRThz94D9lfg7kOfTR_zYfrO6bihvgyrCtHcy19kGZTp5SL3lMoYgFI12FEJTB4D_9P90-Gzncq',
-        'sender_id' => 'AIzaSyAN5uf7VhyJAIQbjNPT0AKcuCWqo3q2BG4',
+        'server_key' => 'AAAAnAeGVHo:APA91bH4xmyS31G9CFPQ5zDozIx79HaL3lE4IKVzPIfFY2VjumXsf8ybfI6aoJOsK-_f5bCHqSNW3fTbe8uvC_ELeAGVqP3nqSvvDxP-mxVePY8viM-6Vy3wrzUtWhek1nxJFmFB2j_G',
+        'sender_id' => 'AIzaSyBMK-3N_edO30dEVVpYyZ5fs2lc558gQZI',
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
