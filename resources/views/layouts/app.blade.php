974194e2ba0a1ec89075cf41bdeeca8577af06ff
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-06 02:35:03 +0200

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,12 +14,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    {{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     @stack('css')
-
-
-
+    
 </head>
 <body>
 <div id="app">
