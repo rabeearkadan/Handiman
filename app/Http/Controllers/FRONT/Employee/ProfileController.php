0281088eb0206d1b2ec4803ff144b1e6b023d3da
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-06 22:54:28 +0200

    no message

--- app/Http/Controllers/FRONT/Employee/ProfileController.php
+++ app/Http/Controllers/FRONT/Employee/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\FRONT\Employee;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
