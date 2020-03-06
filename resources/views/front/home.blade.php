fd22f805210a6df1a05e3766ba299619962d7da3
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-06 18:32:28 +0200

    no message

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,1 +1,28 @@
 
+<h2>Continue as:</h2>
+
+<form >
+    <section>
+        <div class="row">
+            <div class="col-md-6">
+                <input type="radio" id="control_01" name="select" value="1" checked>
+                <label for="control_01">
+                    <h2> Client </h2>
+                    <img class="img-fluid" src="client.png">
+                </label>
+            </div>
+
+            <div class="col-md-6">
+                <input type="radio" id="control_02" name="select" value="2">
+                <label for="control_02">
+                    <h2>Handyman</h2>
+                    <img class="img-fluid" src="client.png">
+                </label>
+            </div>
+        </div>
+    </section>
+    <div class="row justify-content-md-center">
+        <button type="button" class="btn btn-outline-success btn-lg"> Continue </button>
+    </div>
+
+</form>
