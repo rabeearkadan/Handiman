4adf7a5b3a093b8045c7d9d52d7ac24f112012de
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-06 19:56:23 +0200

    no message

---
+++ app/Http/Controllers/FRONT/Handyman/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\FRONT\Client;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    //
+    public function myProfile(){
+        $user = Auth::user();
+        return view('front.client.profile.edit-profile', compact('user'));
+    }
+    public function editPassword(){
+
+    }
+    public function editPayment(){
+        $user = Auth::user();
+        return view('front.client.profile.payment', compact('user'));
+    }
+
+    public function editProfile(){
+
+    }
+
+
+    public function clientProfile($id, $user_id){
+
+    }
+}
--- routes/employee.php
+++ routes/employee.php
@@ -3,3 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/home", 'HomeController@index')->name('employee.home');
+Route::get("/requests", 'HomeController@index')->name('employee.requests');
+Route::get("/jobs", 'HomeController@index')->name('employee.home');
+Route::get("/messages", 'HomeController@index')->name('employee.messages');
+Route::get("/profile", 'ProfileCotroller@index')->name('employee.profile');
+Route::get("/reviews", 'HomeController@index')->name('employee.reviews');
+
+
+
