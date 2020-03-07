4955bd4d09ae2eb40f9894f52b0c1df21a26cf1c
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-07 19:56:29 +0200

    no message

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
 Auth::routes(['verify' => true]);
 Route::get("register", 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('/contact','MailController@contact')->name('mail.contact');
