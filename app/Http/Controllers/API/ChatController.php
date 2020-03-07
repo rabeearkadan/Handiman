6b38400c572fd1abb4306c93a0e0d79704b8f18a
mohammad <itani0369-@hotmail.com>
2020-03-07 23:49:33 +0200

    .

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -27,7 +27,8 @@ class ChatController extends Controller
         $obj[0] = $params['receiver_id'];
         $obj[$params['receiver_id']][0] = false;
         $array[] = $user->message_requests;
-        array_push($user->message_requests, $obj);
+        array_push($array, $obj);
+        $user->message_requests = $array;
 
         $user->save();
         return response()->json(['status' => 'success', 'user' => $user]);
