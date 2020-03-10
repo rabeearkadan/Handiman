81be0e7cca9bf74204fb61bbff31b515974d1363
mohammad <itani0369-@hotmail.com>
2020-03-11 00:55:08 +0200

    .

--- app/Http/Controllers/CMS/HomeController.php
+++ app/Http/Controllers/CMS/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\CMS;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
