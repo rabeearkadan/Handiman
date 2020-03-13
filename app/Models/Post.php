41d2380375bf90f43009cd0a9f049a82eb8daf2c
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-13 23:17:42 +0200

    no message

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,8 +7,8 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
 class Post extends Eloquent
 {
-    public function users(){
-        return $this->belongsToMany(User::class);
+    public function user(){
+        return $this->belongsTo(User::class);
     }
     public function tags(){
         return $this->belongsToMany(Service::class);
