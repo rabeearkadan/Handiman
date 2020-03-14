f6669054b22857bc339df2a82e3aa5138d162fb5
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-14 20:19:45 +0200

    no message

--- resources/views/front/employee/post/index.blade.php
+++ resources/views/front/employee/post/index.blade.php
@@ -6,6 +6,7 @@
     <link href="{{asset('css/employee/posts.css')}}" rel="stylesheet">
     <link href="{{asset('css/employee/post-detail.css')}}" rel="stylesheet">
     <link href="{{asset('css/employee/common-classes.css')}}" rel="stylesheet">
+    <link href="{{asset('css/employee/widgets.css')}}" rel="stylesheet">
 @endpush
 @section('content')
     <div class="page-wrapper">
@@ -20,27 +21,34 @@
                                 </div><!-- /.page-title -->
                                 <div class="posts">
                                     @foreach($user->posts as $post)
-                                    <div class="post post-boxed">
-                                        <div class="post-image">
-                                            <img src="{{config('image.path').$post->image}}" alt="A Clockwork Origin">
-                                            <a class="read-more" href="blog-detail-right-sidebar.html">View</a>
-                                        </div><!-- /.post-image -->
-                                        <div class="post-content">
-                                            <h2><a href="blog-detail.html"> {{$post->title}} </a>
-                                                <div class="pull-right">
-                                                    <a href="#"> <i class="fa fa-edit"></i> </a>
-                                                    <a href="#"> <i class="fa fa-trash"></i> </a>
-                                                </div>
-                                            </h2>
-                                            <p> {{$post->content}} </p>
-                                        </div><!-- /.post-content -->
-                                        <div class="post-meta clearfix">
-                                            <div class="post-meta-date">08/24/2015</div><!-- /.post-meta-date -->
-                                            <div class="post-meta-categories"><i class="fa fa-tags"></i> <a href="blog-detail.html"> Put tags here </a></div><!-- /.post-meta-categories -->
-                                            <div class="post-meta-comments"><i class="fa fa-comments"></i> <a href="blog-detail.html">3 comments</a></div><!-- /.post-meta-comments -->
-                                            <div class="post-meta-more"><a href="blog-detail.html">Read More <i class="fa fa-chevron-right"></i></a></div><!-- /.post-meta-more -->
-                                        </div><!-- /.post-meta -->
-                                    </div><!-- /.post -->
+                                        <div class="post post-boxed">
+                                            <div class="post-image">
+                                                <img src="{{config('image.path').$post->image}}"
+                                                     alt="A Clockwork Origin">
+                                                <a class="read-more" href="blog-detail-right-sidebar.html">View</a>
+                                            </div><!-- /.post-image -->
+                                            <div class="post-content">
+                                                <h2><a href="blog-detail.html"> {{$post->title}} </a>
+                                                    <div class="pull-right">
+                                                        <a href="#"> <i class="fa fa-edit"></i> </a>
+                                                        <a href="#"> <i class="fa fa-trash"></i> </a>
+                                                    </div>
+                                                </h2>
+                                                <p> {{$post->content}} </p>
+                                            </div><!-- /.post-content -->
+                                            <div class="post-meta clearfix">
+                                                <div class="post-meta-date">08/24/2015</div><!-- /.post-meta-date -->
+                                                <div class="post-meta-categories"><i class="fa fa-tags"></i> <a
+                                                        href="blog-detail.html"> Put tags here </a></div>
+                                                <!-- /.post-meta-categories -->
+                                                <div class="post-meta-comments"><i class="fa fa-comments"></i> <a
+                                                        href="blog-detail.html">3 comments</a></div>
+                                                <!-- /.post-meta-comments -->
+                                                <div class="post-meta-more"><a href="blog-detail.html">Read More <i
+                                                            class="fa fa-chevron-right"></i></a></div>
+                                                <!-- /.post-meta-more -->
+                                            </div><!-- /.post-meta -->
+                                        </div><!-- /.post -->
                                     @endforeach
                                 </div><!-- /.posts -->
                                 <div class="pager">
@@ -54,6 +62,25 @@
                                 </div><!-- /.pagination -->
                             </div><!-- /.content -->
                         </div><!-- /.col-* -->
+                        <div class="col-sm-4 col-lg-3">
+                            <div class="sidebar">
+                                <div class="widget">
+                                    <h2 class="widgettitle">Categories</h2>
+
+                                    <ul class="menu">
+                                        <li><a href="#">Automotive</a></li>
+                                        <li><a href="#">Jobs</a></li>
+                                        <li><a href="#">Nightlife</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">Transportation</a></li>
+                                        <li><a href="#">Real Estate</a></li>
+                                        <li><a href="#">Restaurants</a></li>
+                                    </ul><!-- /.menu -->
+                                </div><!-- /.wifget -->
+                            </div><!-- /.sidebar -->
+                        </div><!-- /.col-* -->
+
+
                     </div><!-- /.row -->
                 </div><!-- /.container -->
             </div><!-- /.main-inner -->
