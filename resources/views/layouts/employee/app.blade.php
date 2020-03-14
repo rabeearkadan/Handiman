ff46a1ab8efdfc21ba7c9e6fdd771aa7368e9dc6
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-14 15:13:57 +0200

    no message

--- resources/views/layouts/employee/app.blade.php
+++ resources/views/layouts/employee/app.blade.php
@@ -9,8 +9,8 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
-    @stack('css')
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    @stack('css')
     <link href="{{asset('css/employee/navbar.css')}}" rel="stylesheet">
     <link href="{{asset('css/employee/preloader.css')}}" rel="stylesheet">
 </head>
