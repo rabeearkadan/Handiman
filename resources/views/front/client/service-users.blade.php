0129299989304c174e3596ecae10f1c93fedfdc1
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-16 14:22:41 +0200

    no message

--- resources/views/front/client/service-users.blade.php
+++ resources/views/front/client/service-users.blade.php
@@ -87,7 +87,7 @@
                                 </div><!-- /.card-row-image -->
                                 <div class="card-row-body">
                                     <h2 class="card-row-title">
-                                        <a href=""> {{$user->name}} </a>
+                                        <a href="{{route('client.user-profile',[$service->id,$user->id])}}">{{$user->name}} </a>
                                     </h2>
                                     <div class="card-row-content">
                                         <p> {{$user->biography}} </p>
