b950d1f819055d411af4cb18015fc8e557be779c
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-16 17:00:19 +0200

    no message

--- resources/views/front/client/profile/edit-profile.blade.php
+++ resources/views/front/client/profile/edit-profile.blade.php
@@ -3,30 +3,24 @@
     <div class="page-title">
         <h1>Edit Profile</h1>
     </div><!-- /.page-title -->
-
     <div class="background-white p20 mb30">
         <h4 class="page-title">
             Contact Information
-
             <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
         </h4>
-
         <div class="row">
             <div class="form-group col-sm-6">
                 <label>Name</label>
                 <input type="text" class="form-control" value="John">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-6">
                 <label>Surname</label>
                 <input type="text" class="form-control" value="Doe">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-6">
                 <label>E-mail</label>
                 <input type="text" class="form-control" value="sample@example.com">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-6">
                 <label>Phone</label>
                 <input type="text" class="form-control" value="123-456-789">
@@ -34,72 +28,54 @@
         </div><!-- /.row -->
     </div>
 
-    <div class="background-white p20 mb30">
-        <h4 class="page-title">
-            Social Connections
-
-            <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
-        </h4>
-
-        <div class="form-horizontal">
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Facebook</label>
-
-                <div class="col-sm-9">
-                    <input type="text" class="form-control" value="http://facebook.com/">
-                </div><!-- /.col-* -->
-            </div><!-- /.form-group -->
-
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Twitter</label>
-
-                <div class="col-sm-9">
-                    <input type="text" class="form-control" value="http://twitter.com/">
-                </div><!-- /.col-* -->
-            </div><!-- /.form-group -->
-
-
-
-
-
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Instagram</label>
-
-                <div class="col-sm-9">
-                    <input type="text" class="form-control" value="http://instagram.com/">
-                </div><!-- /.col-* -->
-            </div><!-- /.form-group -->
-        </div><!-- /.form-inline -->
-    </div><!-- /.background-white -->
+    <div class="background-white p30 mb30">
+        <h3 class="page-title">Attributes</h3>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <select name="property">
+                        <option>Property Type</option>
+                        <option>Apartment</option>
+                        <option>Condo</option>
+                        <option>House</option>
+                        <option>Villa</option>
+                    </select>
+                </div><!-- /.form-group -->
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <select name="contract">
+                        <option>Contract</option>
+                        <option>Rent</option>
+                        <option>Sale</option>
+                    </select>
+                </div><!-- /.form-group -->
+            </div><!-- /.col-* -->
+        </div><!-- /.row -->
+    </div><!-- /.box -->
 
     <div class="background-white p20 mb30">
         <h4 class="page-title">
             Address
-
             <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
         </h4>
-
         <div class="row">
             <div class="form-group col-sm-6">
                 <label>State</label>
                 <input type="text" class="form-control" value="New York">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-6">
                 <label>City</label>
                 <input type="text" class="form-control" value="New York City">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-6">
                 <label>Street</label>
                 <input type="text" class="form-control" value="Everton Eve">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-3">
                 <label>House Number</label>
                 <input type="text" class="form-control" value="123">
             </div><!-- /.form-group -->
-
             <div class="form-group col-sm-3">
                 <label>ZIP</label>
                 <input type="text" class="form-control" value="12345">
@@ -110,10 +86,9 @@
     <div class="background-white p20 mb30">
         <h4 class="page-title">
             Biography
-
             <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
         </h4>
-
-        <textarea class="form-control" rows="7"></textarea><div class="textarea-resize"></div>
+        <textarea class="form-control" rows="7"></textarea>
+        <div class="textarea-resize"></div>
     </div>
 @endsection
--- resources/views/front/client/profile/my-profile.blade.php
+++ resources/views/front/client/profile/my-profile.blade.php
@@ -20,22 +20,20 @@
                                 <div class="widget">
                                     <div class="user-photo">
                                         <a href="#">
-                                            <img src="assets/img/tmp/agent-2.jpg" alt="User Photo">
-                                            <span class="user-photo-action">Click here to reupload</span>
+                                            <img src="" alt="User Photo">
+                                            <span class="user-photo-action">Click here to re-upload</span>
                                         </a>
                                     </div><!-- /.user-photo -->
                                 </div><!-- /.widget -->
                                 <div class="widget">
                                     <ul class="menu-advanced">
-                                        <li class="@if(request()->is('client/profile')) {{'active'}} @endif"><a href="{{route('client.profile')}}"><i class="fa fa-user"></i> Edit Profile</a></li>
-                                        <li class="@if(request()->is('client/profile/password')) {{'active'}} @endif"><a href="{{route('client.password')}}"><i class="fa fa-key"></i> Password</a></li>
-                                        <li class="@if(request()->is('client/profile/payment')) {{'active'}} @endif"><a href="{{route('client.payment')}}"><i class="fa fa-money"></i> Payment</a></li>
+                                        <li class="@if(request()->is('client/profile')) {{'active'}} @endif"><a href="{{route('client.profile')}}"><i class="fa fa-user"></i> Edit Profile </a></li>
+                                        <li class="@if(request()->is('client/profile/password')) {{'active'}} @endif"><a href="{{route('client.password')}}"><i class="fa fa-key"></i> Security </a></li>
+                                        <li class="@if(request()->is('client/profile/payment')) {{'active'}} @endif"><a href="{{route('client.payment')}}"><i class="fa fa-money"></i> Payment </a></li>
                                     </ul>
                                 </div><!-- /.widget -->
-
                             </div><!-- /.sidebar -->
                         </div><!-- /.col-* -->
-
                         <div class="col-sm-8 col-lg-9">
                             <div class="content">
                                 @yield('profile-content')
