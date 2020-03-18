8594532af459122d7196f7bc50a070c06d542256
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-19 01:43:31 +0200

    no message

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Auth::routes(['verify' => true]);
 Auth::routes();
 
 Route::get("register", 'Auth\RegisterController@showRegistrationForm')->name('register');
