209226bab9f61be33edfb642d83b8a34fa334027
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-19 17:26:14 +0200

    no message

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -21,25 +21,25 @@
     <div class="container">
         <div class="panel pricing-table">
             <div class="pricing-plan">
-                <img src="https://s22.postimg.cc/8mv5gn7w1/paper-plane.png" alt="" class="pricing-img">
+                <img src="/public/images/client-home.png" alt="" class="pricing-img">
                 <h2 class="pricing-header"> Client </h2>
                 <ul class="pricing-features">
                     <li class="pricing-features-item"> Thousands of verified professionals</li>
-                    <li class="pricing-features-item"> Guranteed Services quality</li>
+                    <li class="pricing-features-item"> Guaranteed Services quality</li>
                 </ul>
-                <span class="pricing-price"> Something here </span>
-                <a href="#" class="pricing-button"> Continue </a>
+                <span class="pricing-price"> Waiting for you </span>
+                <a href="{{route('client.home')}}" class="pricing-button"> Continue </a>
             </div>
 
             <div class="pricing-plan">
-                <img src="https://s28.postimg.cc/ju5bnc3x9/plane.png" alt="" class="pricing-img">
+                <img src="/public/images/employee-home.png" alt="" class="pricing-img">
                 <h2 class="pricing-header"> Handyman </h2>
                 <ul class="pricing-features">
                     <li class="pricing-features-item">Custom Schedule</li>
                     <li class="pricing-features-item">Multiple clients waiting for you</li>
                 </ul>
                 <span class="pricing-price">Let's go</span>
-                <a href="#" class="pricing-button is-featured"> Continue </a>
+                <a href="{{route('employee.home')}}" class="pricing-button is-featured"> Continue </a>
             </div>
         </div>
     </div>
