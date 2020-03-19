c61847c1fd3daec96ef2be96f131614122de95ea
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-19 17:09:22 +0200

    no message

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,28 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<h2>Continue as:</h2>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-<form >
-    <section>
-        <div class="row">
-            <div class="col-md-6">
-                <input type="radio" id="control_01" name="select" value="1" checked>
-                <label for="control_01">
-                    <h2> Client </h2>
-                    <img class="img-fluid" src="client.png">
-                </label>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link href="{{asset('css/user-employee.css')}}" rel="stylesheet">
+
+</head>
+<body>
+<div class="background">
+    <div class="container">
+        <div class="panel pricing-table">
+            <div class="pricing-plan">
+                <img src="https://s22.postimg.cc/8mv5gn7w1/paper-plane.png" alt="" class="pricing-img">
+                <h2 class="pricing-header"> Client </h2>
+                <ul class="pricing-features">
+                    <li class="pricing-features-item"> Thousands of verified professionals</li>
+                    <li class="pricing-features-item"> Guranteed Services quality</li>
+                </ul>
+                <span class="pricing-price"> Something here </span>
+                <a href="#" class="pricing-button"> Continue </a>
             </div>
 
-            <div class="col-md-6">
-                <input type="radio" id="control_02" name="select" value="2">
-                <label for="control_02">
-                    <h2>Handyman</h2>
-                    <img class="img-fluid" src="client.png">
-                </label>
+            <div class="pricing-plan">
+                <img src="https://s28.postimg.cc/ju5bnc3x9/plane.png" alt="" class="pricing-img">
+                <h2 class="pricing-header"> Handyman </h2>
+                <ul class="pricing-features">
+                    <li class="pricing-features-item">Custom Schedule</li>
+                    <li class="pricing-features-item">Multiple clients waiting for you</li>
+                </ul>
+                <span class="pricing-price">Let's go</span>
+                <a href="#" class="pricing-button is-featured"> Continue </a>
             </div>
         </div>
-    </section>
-    <div class="row justify-content-md-center">
-        <button type="button" class="btn btn-outline-success btn-lg"> Continue </button>
     </div>
+</div>
 
-</form>
+
+</body>
+</html>
