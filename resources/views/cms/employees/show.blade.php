70319e0cd616153c63f1fde9f21707442cd7b9a6
mohammad <itani0369-@hotmail.com>
2020-03-19 18:38:29 +0200

    Update show.blade.php

--- resources/views/cms/employees/show.blade.php
+++ resources/views/cms/employees/show.blade.php
@@ -32,7 +32,7 @@
             font-size: 1.17em;
             margin-block-start: 1em;
             margin-block-end: 1em;
-            margin-inline-start: 0px;
+            margin-inline-start: 10px;
             margin-inline-end: 0px;
             font-weight: bold;
         }
