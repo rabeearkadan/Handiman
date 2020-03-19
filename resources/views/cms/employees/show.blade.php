487971d6a3ea75b77361abed6dc42b541fcdf28a
mohammad <itani0369-@hotmail.com>
2020-03-19 22:20:10 +0200

    Update show.blade.php

--- resources/views/cms/employees/show.blade.php
+++ resources/views/cms/employees/show.blade.php
@@ -8,25 +8,55 @@
         <div class="col-lg-3 col-6">
 
             <div class="small-box bg-success">
-                <div class="small-box bg-success">
-                    <div class="inner">
-                        <h3> test</h3>
-                        <p>name</p>
-                        <p> This profile belongs to {{$employee->name}}</p>
-                    </div>
-
-                    <div class="icon">
-                        <i class="ion ion-bookmark"></i>
-                    </div>
+                <div class="inner">
+                    <h3> test</h3>
+                    <p>name</p>
+                    <p> This profile belongs to {{$employee->name}}</p>
                 </div>
+
+                <div class="icon">
+                    <i class="ion ion-bookmark"></i>
+                </div>
+
             </div>
         </div>
+
+        <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3> test</h3>
+                    <p>name</p>
+                    <p> This profile belongs to {{$employee->email}}</p>
+                </div>
+
+                <div class="icon">
+                    <i class="ion ion-bookmark"></i>
+                </div>
+
+            </div>
+        </div>
+
+
     </div>
 @endsection
 
 
 @push('css')
     <style>
+        .row {
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            margin-right: -7.5px;
+            margin-left: -7.5px;
+        }
+
+        *, ::after, ::before {
+            box-sizing: border-box;
+        }
+
         h3 {
             display: block;
             font-size: 1.17em;
@@ -36,6 +66,7 @@
             margin-inline-end: 0px;
             font-weight: bold;
         }
+
         .small-box h3, .small-box p {
             z-index: 5;
         }
@@ -44,10 +75,20 @@
             font-size: 1rem;
             margin-left: 10px;
         }
+
+        .bg-info, .bg-info > a {
+            color: #fff !important;
+        }
+
+        .bg-info {
+            background-color: #17a2b8 !important;
+        }
+
         p {
             margin-top: 0;
             margin-bottom: 1rem;
         }
+
         *, ::after, ::before {
             box-sizing: border-box;
         }
@@ -59,20 +100,23 @@
             margin-inline-start: 0px;
             margin-inline-end: 0px;
         }
-        .bg-success, .bg-success>a {
-            color: #fff!important;
+
+        .bg-success, .bg-success > a {
+            color: #fff !important;
         }
 
         .bg-success {
-            background-color: #28a745!important;
+            background-color: #28a745 !important;
         }
+
         .small-box {
             border-radius: .25rem;
-            box-shadow: 0 0 1px rgba(0,0,0,.125), 0 1px 3px rgba(0,0,0,.2);
+            box-shadow: 0 0 1px rgba(0, 0, 0, .125), 0 1px 3px rgba(0, 0, 0, .2);
             display: block;
             margin-bottom: 20px;
             position: relative;
         }
+
         .small-box .icon > i {
             font-size: 90px;
             position: absolute;
