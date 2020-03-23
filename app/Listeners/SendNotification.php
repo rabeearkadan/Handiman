81b7a37a5c18200d64d776df7d79bc2cf1cbe977
mohammad <itani0369-@hotmail.com>
2020-03-24 01:13:02 +0200

    Update SendNotification.php

--- app/Listeners/SendNotification.php
+++ app/Listeners/SendNotification.php
@@ -86,6 +86,7 @@ class SendNotification
             //return Array - you should try to resend the message to the tokens in the array
             $downstreamResponse->tokensToRetry();
             Log::info($downstreamResponse->numberSuccess());
+            dd($downstreamResponse);
             return $downstreamResponse->numberSuccess();
         } catch (\Exception $e) {
             return false;
