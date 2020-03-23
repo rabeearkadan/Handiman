06eaf1a88eb52c3bcb7e789a043867c50b9bcc86
mohammad <itani0369-@hotmail.com>
2020-03-24 01:38:51 +0200

    Update fcm.php

--- config/fcm.php
+++ config/fcm.php
@@ -6,7 +6,7 @@ return [
 
     'http' => [
         'server_key' => 'AAAAnAeGVHo:APA91bH4xmyS31G9CFPQ5zDozIx79HaL3lE4IKVzPIfFY2VjumXsf8ybfI6aoJOsK-_f5bCHqSNW3fTbe8uvC_ELeAGVqP3nqSvvDxP-mxVePY8viM-6Vy3wrzUtWhek1nxJFmFB2j_G',
-        'sender_id' => '670141142138',
+        'sender_id' => 'AIzaSyBMK-3N_edO30dEVVpYyZ5fs2lc558gQZI',
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
