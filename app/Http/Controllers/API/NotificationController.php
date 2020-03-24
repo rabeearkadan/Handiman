4f1e92c9b3105714767b5107846fd2951ad501ce
mohammad <itani0369-@hotmail.com>
2020-03-24 02:32:10 +0200

    Update NotificationController.php

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -29,4 +29,23 @@ class NotificationController extends Controller
 
         return response()->json(['status' => 'success', 'notification' => $notification]);
     }
+
+    public function makeMessage( $user_id, $isClient, Request $request){
+
+        $notification = array();
+
+        $user = User::query()->find($user_id);
+
+        $notification['to'] = $isClient == 1?$user->client_device_token:$user->employee_device_token;
+        $notification['user'] = "admin";
+        $notification['message'] = "hello";
+        $notification['image']=$user->image;
+        $notification['email']=$user->email;
+        $notification['type'] = 'comment';// maybe "notification", "comment(message)", "request","message"
+        $notification['object'] = [];
+
+        event(new NotificationSenderEvent($notification));
+
+        return response()->json(['status' => 'success', 'notification' => $notification]);
+    }
 }
