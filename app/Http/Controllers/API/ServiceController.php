59d75b61877ee40886856eb96b39710c9922421a
mohammad <itani0369-@hotmail.com>
2020-03-29 21:15:12 +0300

    Update ServiceController.php

--- app/Http/Controllers/API/ServiceController.php
+++ app/Http/Controllers/API/ServiceController.php
@@ -38,11 +38,10 @@ class ServiceController extends Controller
 
         $user = User::query()->find(Auth::id());
 
-        $service_ids_in_user = array($user->service_ids);
+        $service_ids_in_user = $user->service_ids;
         $key = 0;
 
         for ($i = 0; $i < sizeof($user->service_ids); $i++) {
-//          dd($service_ids_in_user[$i]);
             if ($service_ids_in_user[$i] === $id) {
                 $key = $i;
                 // dd($key);
@@ -52,9 +51,9 @@ class ServiceController extends Controller
         $user->service_ids = $service_ids_in_user;
         $user->save();
 
-
+//wan l mshkli wen am saving it back again its saved as an objcect //try now its the same
         $service = Service::query()->findOrFail($id);
-        $user_ids_in_service = array($service->user_ids);
+        $user_ids_in_service =$service->user_ids;
         $key = 0;
         for ($i = 0; $i < sizeof($user_ids_in_service); $i++) {
             if ($user_ids_in_service[$i] == $user->id) {
