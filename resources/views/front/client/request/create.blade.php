9a0a2572dc8b6343ed7d293c321377067314e507
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-31 17:39:36 +0300

    no message

--- resources/views/front/client/request/create.blade.php
+++ resources/views/front/client/request/create.blade.php
@@ -11,9 +11,124 @@
         .pull-right{
             float: right;
         }
-        #date-input:focus:not([readonly]) {
-            border-bottom: 1px solid #26a69a;
-            box-shadow: 0 1px 0 0 #26a69a;
+        .select-box {
+            position: relative;
+            display: block;
+            width: 100%;
+            margin: 0 auto;
+            font-family: 'Open Sans', 'Helvetica Neue', 'Segoe UI', 'Calibri', 'Arial', sans-serif;
+            font-size: 18px;
+            color: #60666d;
+        }
+        @media (min-width: 768px) {
+            .select-box {
+                width: 70%;
+            }
+        }
+        @media (min-width: 992px) {
+            .select-box {
+                width: 50%;
+            }
+        }
+        @media (min-width: 1200px) {
+            .select-box {
+                width: 30%;
+            }
+        }
+        .select-box__current {
+            position: relative;
+            box-shadow: 0 15px 30px -10px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            outline: none;
+        }
+        .select-box__current:focus + .select-box__list {
+            opacity: 1;
+            -webkit-animation-name: none;
+            animation-name: none;
+        }
+        .select-box__current:focus + .select-box__list .select-box__option {
+            cursor: pointer;
+        }
+        .select-box__current:focus .select-box__icon {
+            -webkit-transform: translateY(-50%) rotate(180deg);
+            transform: translateY(-50%) rotate(180deg);
+        }
+        .select-box__icon {
+            position: absolute;
+            top: 50%;
+            right: 15px;
+            -webkit-transform: translateY(-50%);
+            transform: translateY(-50%);
+            width: 20px;
+            opacity: 0.3;
+            -webkit-transition: 0.2s ease;
+            transition: 0.2s ease;
+        }
+        .select-box__value {
+            display: -webkit-box;
+            display: flex;
+        }
+        .select-box__input {
+            display: none;
+        }
+        .select-box__input:checked + .select-box__input-text {
+            display: block;
+        }
+        .select-box__input-text {
+            display: none;
+            width: 100%;
+            margin: 0;
+            padding: 15px;
+            background-color: #fff;
+        }
+        .select-box__list {
+            position: absolute;
+            width: 100%;
+            padding: 0;
+            list-style: none;
+            opacity: 0;
+            -webkit-animation-name: HideList;
+            animation-name: HideList;
+            -webkit-animation-duration: 0.5s;
+            animation-duration: 0.5s;
+            -webkit-animation-delay: 0.5s;
+            animation-delay: 0.5s;
+            -webkit-animation-fill-mode: forwards;
+            animation-fill-mode: forwards;
+            -webkit-animation-timing-function: step-start;
+            animation-timing-function: step-start;
+            box-shadow: 0 15px 30px -10px rgba(0, 0, 0, 0.1);
+        }
+        .select-box__option {
+            display: block;
+            padding: 15px;
+            background-color: #fff;
+        }
+        .select-box__option:hover, .select-box__option:focus {
+            color: #546c84;
+            background-color: #fbfbfb;
+        }
+
+        @-webkit-keyframes HideList {
+            from {
+                -webkit-transform: scaleY(1);
+                transform: scaleY(1);
+            }
+            to {
+                -webkit-transform: scaleY(0);
+                transform: scaleY(0);
+            }
+        }
+
+        @keyframes HideList {
+            from {
+                -webkit-transform: scaleY(1);
+                transform: scaleY(1);
+            }
+            to {
+                -webkit-transform: scaleY(0);
+                transform: scaleY(0);
+            }
         }
     </style>
 @endpush
@@ -53,11 +168,24 @@
                                     </div><!-- /.col-* -->
                                     <div class="col-sm-5">
                                         <div class="form-group">
-                                            <label for="address">Address</label>
-                                            <select id="address">
-                                                <option> address 1</option>
-                                                <option> address 2</option>
-                                            </select>
+
+
+                                            <div class="select-box">
+                                                <div class="select-box__current" tabindex="1">
+                                                    <div class="select-box__value">
+                                                        <input class="select-box__input" type="radio" id="0" value="1" name="address" checked="checked"/>
+                                                        <p class="select-box__input-text">Address 1</p>
+                                                    </div>
+
+                                                    <img class="select-box__icon" src="/public/images/client/drop-down-arrow.svg" alt="Arrow Icon" aria-hidden="true"/>
+                                                </div>
+                                                <ul class="select-box__list">
+                                                    <li>
+                                                        <label class="select-box__option" for="0" aria-hidden="aria-hidden"> Address1</label>
+                                                    </li>
+                                                </ul>
+                                            </div>
+
                                         </div><!-- /.form-group -->
                                     </div><!-- /.col-* -->
                                 </div><!-- /.row -->
