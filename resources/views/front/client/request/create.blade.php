f364282dfd93e4aa96a3600290ca7a2cf39d3d59
ahmaddorra <ahmadeldorra1@gmail.com>
2020-03-31 19:48:16 +0300

    no message

--- resources/views/front/client/request/create.blade.php
+++ resources/views/front/client/request/create.blade.php
@@ -9,9 +9,10 @@
     <link href="{{asset('css/client/requests/icons.css')}}" rel="stylesheet" media="screen">
     <link href="{{asset('lib/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
     <style>
-        .pull-right{
+        .pull-right {
             float: right;
         }
+
         .select-box {
             position: relative;
             display: block;
@@ -30,18 +31,22 @@
             outline-style: inset;
             outline-width: medium;
         }
+
         .select-box__current:focus + .select-box__list {
             opacity: 1;
             -webkit-animation-name: none;
             animation-name: none;
         }
+
         .select-box__current:focus + .select-box__list .select-box__option {
             cursor: pointer;
         }
+
         .select-box__current:focus .select-box__icon {
             -webkit-transform: translateY(-50%) rotate(180deg);
             transform: translateY(-50%) rotate(180deg);
         }
+
         .select-box__icon {
             position: absolute;
             top: 50%;
@@ -53,16 +58,20 @@
             -webkit-transition: 0.2s ease;
             transition: 0.2s ease;
         }
+
         .select-box__value {
             display: -webkit-box;
             display: flex;
         }
+
         .select-box__input {
             display: none;
         }
+
         .select-box__input:checked + .select-box__input-text {
             display: block;
         }
+
         .select-box__input-text {
             display: none;
             width: 100%;
@@ -70,6 +79,7 @@
             padding: 15px;
             background-color: #fff;
         }
+
         .select-box__list {
             position: absolute;
             width: 100%;
@@ -88,11 +98,13 @@
             animation-timing-function: step-start;
             box-shadow: 0 15px 30px -10px rgba(0, 0, 0, 0.1);
         }
+
         .select-box__option {
             display: block;
             padding: 15px;
             background-color: #fff;
         }
+
         .select-box__option:hover, .select-box__option:focus {
             color: #546c84;
             background-color: #fbfbfb;
@@ -128,26 +140,30 @@
             <div class="main-inner">
                 <div class="container">
                     <div class="content">
-                        @if($employee)
-                            <div class="contact-form-wrapper clearfix background-white p30">
-                                <div class="row">
-                                    <div class="col-sm-2">
-                                        <img src="{{config('image.path').$employee->image}}" class="img-thumbnail"
-                                             alt="employee">
-                                        <h2>
-                                            {{$employee->name}}
-                                        </h2>
-                                    </div>
-                                    <div class="col-sm-10">
-                                        <h3>will put employee info later here</h3>
+                        <form class="contact-form" method="post" action="{{route('client.request.store')}}">
+                            @csrf
+                            @if($employee)
+                                <div class="contact-form-wrapper clearfix background-white p30">
+                                    <div class="row">
+                                        <div class="col-sm-2">
+                                            <img src="{{config('image.path').$employee->image}}" class="img-thumbnail"
+                                                 alt="employee">
+                                            <h2>
+                                                {{$employee->name}}
+                                            </h2>
+                                        </div>
+                                        <div class="col-sm-10">
+                                            <h3>will put employee info later here</h3>
+                                            <input type="text" name="employee_id" value="{{$employee->id}}"
+                                                   style="display: none">
+                                            <input type="text" name="service_id" value="{{$service->id}}"
+                                                   style="display: none">
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endif
-                        <h3> The more you elaborate, the more we can help!</h3>
-                        <div class="contact-form-wrapper clearfix background-white p30">
-                            <form class="contact-form" method="post" action="{{route('client.request.store',['service_id'=>$service->id,'employee_id'=>$employee->id])}}">
-                                @csrf
+                            @endif
+                            <h3> The more you elaborate, the more we can help!</h3>
+                            <div class="contact-form-wrapper clearfix background-white p30">
                                 <div class="row">
                                     <div class="col-sm-5">
                                         <div class="form-group">
@@ -161,15 +177,19 @@
                                             <div class="select-box">
                                                 <div class="select-box__current" tabindex="1">
                                                     <div class="select-box__value">
-                                                        <input class="select-box__input" type="radio" id="0" value="1" name="address" checked="checked"/>
+                                                        <input class="select-box__input" type="radio" id="0" value="1"
+                                                               name="address" checked="checked"/>
                                                         <p class="select-box__input-text">Address 1</p>
                                                     </div>
 
-                                                    <img class="select-box__icon" src="/public/images/client/drop-down-arrow.svg" alt="Arrow Icon" aria-hidden="true"/>
+                                                    <img class="select-box__icon"
+                                                         src="/public/images/client/drop-down-arrow.svg"
+                                                         alt="Arrow Icon" aria-hidden="true"/>
                                                 </div>
                                                 <ul class="select-box__list">
                                                     <li>
-                                                        <label class="select-box__option" for="0" aria-hidden="true"> Address1</label>
+                                                        <label class="select-box__option" for="0" aria-hidden="true">
+                                                            Address1</label>
                                                     </li>
                                                 </ul>
                                             </div>
@@ -179,19 +199,21 @@
                                 </div><!-- /.row -->
                                 <div class="form-group">
                                     <label for="description"> Problem Description</label>
-                                    <textarea class="form-control" id="description" name="description" rows="6"></textarea>
+                                    <textarea class="form-control" id="description" name="description"
+                                              rows="6"></textarea>
                                 </div><!-- /.form-group -->
                                 <div class="row">
                                     <div class="col-sm-5">
                                         <div class="form-group">
                                             <label for="date-input"> Pick a day </label>
-                                            <input id="date-input" name="date" type="text" data-dd-theme="leaf" data-dd-format="Y-m-d">
+                                            <input id="date-input" name="date" type="text" data-dd-theme="leaf"
+                                                   data-dd-format="Y-m-d">
                                         </div><!--/.form-group-->
                                     </div><!--/.col-*-->
                                 </div><!--/.row-->
                                 <button type="submit" class="btn btn-primary pull-right"> Request</button>
-                            </form><!-- /.form -->
-                        </div><!-- /.wrapper -->
+                            </div><!-- /.wrapper -->
+                        </form><!-- /.form -->
                     </div><!-- /.content -->
                 </div><!-- /.container -->
             </div><!-- /.main-inner -->
