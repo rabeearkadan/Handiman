2818f1b40cb007856ef432f3a2e519b7b69179e4
ahmaddorra <ahmadeldorra1@gmail.com>
2020-04-01 18:18:37 +0300

    no message

--- resources/views/front/client/profile/edit-profile.blade.php
+++ resources/views/front/client/profile/edit-profile.blade.php
@@ -223,10 +223,7 @@
             var searchBox = new google.maps.places.SearchBox(input);
             map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-            var place = input.getPlace();
-
-            document.getElementById('lat').value = place.geometry.location.lat();
-            document.getElementById('long').value = place.geometry.location.lng();
+            
             // Bias the SearchBox results towards current map's viewport.
             map.addListener('bounds_changed', function () {
                 searchBox.setBounds(map.getBounds());
