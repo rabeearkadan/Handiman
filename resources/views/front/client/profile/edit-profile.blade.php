373aab29c435acfa3f2356c600da221a019c9f49
ahmaddorra <ahmadeldorra1@gmail.com>
2020-04-01 19:44:55 +0300

    no message

--- resources/views/front/client/profile/edit-profile.blade.php
+++ resources/views/front/client/profile/edit-profile.blade.php
@@ -2,86 +2,6 @@
 @push('css')
     <link href="{{asset('css/client/map.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/client/new-address.css')}}" rel="stylesheet" type="text/css">
-    <style>
-        /* Always set the map height explicitly to define the size of the div
-         * element that contains the map. */
-        #map {
-        }
-
-        /* Optional: Makes the sample page fill the window. */
-
-        #description {
-            font-family: Roboto, serif;
-            font-size: 15px;
-            font-weight: 300;
-        }
-
-        #infowindow-content .title {
-            font-weight: bold;
-        }
-
-        #infowindow-content {
-            display: none;
-        }
-
-        #map #infowindow-content {
-            display: inline;
-        }
-
-        .pac-card {
-            margin: 10px 10px 0 0;
-            border-radius: 2px 0 0 2px;
-            box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            outline: none;
-            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-            background-color: #fff;
-            font-family: Roboto, serif;
-        }
-
-        #pac-container {
-            padding-bottom: 12px;
-            margin-right: 12px;
-        }
-
-        .pac-controls {
-            display: inline-block;
-            padding: 5px 11px;
-        }
-
-        .pac-controls label {
-            font-family: Roboto, serif;
-            font-size: 13px;
-            font-weight: 300;
-        }
-
-        #pac-input {
-            background-color: #fff;
-            font-family: Roboto, serif;
-            font-size: 15px;
-            font-weight: 300;
-            margin-left: 12px;
-            padding: 0 11px 0 13px;
-            text-overflow: ellipsis;
-            width: 400px;
-        }
-
-        #pac-input:focus {
-            border-color: #4d90fe;
-        }
-
-        #title {
-            color: #fff;
-            background-color: #4d90fe;
-            font-size: 25px;
-            font-weight: 500;
-            padding: 6px 12px;
-        }
-
-        #target {
-            width: 345px;
-        }
-    </style>
 @endpush
 @section('profile-content')
     <div class="page-title">
@@ -113,15 +33,12 @@
     </div>
 
 
-
-
+@if(count($user->locations) > 0)
+@foreach($user->locations as $location)
     <div class="background-white p20 mb30">
-        <form method="post"
-              action="@if(($user->locations)) {{route('client.edit.address')}}  @else {{route('client.add.address')}} @endif">
+        <form method="post" action="{{route('client.edit.address')}}">
             @csrf
-            @if($user->locations)
             @method('put')
-            @endif
             <h4 class="page-title">
                 Address
                 <button type="submit" class="btn btn-primary btn-xs pull-right">Save</button>
@@ -177,8 +94,70 @@
             </div><!-- /.row -->
         </form>
     </div><!-- /.background -white -->
+@endforeach
+@else
+    <div class="background-white p20 mb30">
+        <form method="post" action="{{route('client.add.address')}}">
+            @csrf
+            <h4 class="page-title">
+                Address
+                <button type="submit" class="btn btn-primary btn-xs pull-right">Save</button>
+            </h4>
+            <div class="row">
+                <div class="form-group col-sm-6">
+                    <label for="address_name">Address Name, this will be displayed when choosing your location in a
+                        request </label>
+                    <input type="text" class="form-control" name="name" id="address_name" placeholder="Home Beirut">
+                </div>
+            </div>
+            <div class="map-position">
+                <input id="pac-input" name="map-input" class="controls" type="text" placeholder="Search Box">
+                <input type="hidden" name="lat" id="lat">
+                <input type="hidden" name="long" id="long">
+                <div id="map"></div>
+            </div><!-- /.map-property -->
+            <div class="row" style="margin-top:20px">
+                <div class="form-group col-sm-6">
+                    <label for="street">Street</label>
+                    <input type="text" class="form-control" name="street" id="street" value="" placeholder="">
+                </div><!-- /.form-group -->
+                <div class="form-group col-sm-3">
+                    <label for="house"> House Number/Name </label>
+                    <input type="text" class="form-control" name="house" id="house" value="" placeholder="">
+                </div><!-- /.form-group -->
+                <div class="form-group col-sm-3">
+                    <label for="zip"> ZIP </label>
+                    <input type="text" class="form-control" name="zip" id="zip" value="" placeholder="">
+                </div><!-- /.form-group -->
+            </div><!-- /.row -->
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <select name="property">
+                            <option>Property Type</option>
+                            <option>Apartment</option>
+                            <option>Condo</option>
+                            <option>House</option>
+                            <option>Villa</option>
+                        </select>
+                    </div><!-- /.form-group -->
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <select name="contract">
+                            <option>Contract</option>
+                            <option>Rent</option>
+                            <option>Sale</option>
+                        </select>
+                    </div><!-- /.form-group -->
+                </div><!-- /.col-* -->
+            </div><!-- /.row -->
+        </form>
+    </div><!-- /.background -white -->
+@endif
+
     <div class="line-item__add">
-        <button type="button" class="js-line-item-trigger marketing-button--skin-reset button--icon" id="AddLineItem">
+        <button type="button" onclick="" class="js-line-item-trigger marketing-button--skin-reset button--icon" id="AddLineItem">
             <svg class="icon icon--fill-primary" aria-hidden="true" focusable="false">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
                     <path
@@ -192,14 +171,14 @@
   </span>
         </button>
     </div>
-    <div class="background-white p20 mb30">
-        <h4 class="page-title">
-            Biography
-            <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
-        </h4>
-        <textarea class="form-control" rows="7"></textarea>
-        <div class="textarea-resize"></div>
-    </div>
+{{--    <div class="background-white p20 mb30">--}}
+{{--        <h4 class="page-title">--}}
+{{--            Biography--}}
+{{--            <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>--}}
+{{--        </h4>--}}
+{{--        <textarea class="form-control" rows="7"></textarea>--}}
+{{--        <div class="textarea-resize"></div>--}}
+{{--    </div>--}}
 @endsection
 @push('js')
     <script>
