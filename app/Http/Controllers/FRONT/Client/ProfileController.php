55245a94bc7cf600eaba903ebbcbf91d1fb9820b
ahmaddorra <ahmadeldorra1@gmail.com>
2020-04-02 15:28:29 +0300

    no message

--- app/Http/Controllers/FRONT/Client/ProfileController.php
+++ app/Http/Controllers/FRONT/Client/ProfileController.php
@@ -14,6 +14,7 @@ class ProfileController extends Controller
     //
     public function myProfile(){
         $user = Auth::user();
+        dd($user->client_addresses);
         return view('front.client.profile.edit-profile', compact('user'));
     }
     public function editPassword(){
