8f8b0b11b0ee27cdd3331595706ac20ec9bf902c
mohammad <itani0369-@hotmail.com>
2020-04-02 05:31:10 +0300

    Update RequestController.php

--- app/Http/Controllers/API/RequestController.php
+++ app/Http/Controllers/API/RequestController.php
@@ -244,6 +244,7 @@ class RequestController extends Controller
     {
 
         $requests = Auth::user()->employeeRequests()->where('status','pending')->get();
+        dd($requests);
         if ($requests == null)
             return response()->json(['status' => 'success', 'message' => 'You have no ongoing requests']);
         $requests = $requests->map(function ($item) {
