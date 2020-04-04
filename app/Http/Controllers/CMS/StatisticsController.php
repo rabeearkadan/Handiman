e4dbbc61ce4c9c6d8f97dc97f9b3f5121e499215
mohammad <itani0369-@hotmail.com>
2020-04-05 02:06:21 +0300

    Update StatisticsController.php

--- app/Http/Controllers/CMS/StatisticsController.php
+++ app/Http/Controllers/CMS/StatisticsController.php
@@ -10,6 +10,16 @@ class StatisticsController extends Controller
 {
     public function index()
     {
-        return view('cms.statistics.index');
+
+
+        $dataPoints = array(
+            array("label" => "Chrome", "y" => 64.02),
+            array("label" => "Firefox", "y" => 12.55),
+            array("label" => "IE", "y" => 8.47),
+            array("label" => "Safari", "y" => 6.08),
+            array("label" => "Edge", "y" => 4.29),
+            array("label" => "Others", "y" => 4.59)
+        );
+        return view('cms.statistics.index', compact($dataPoints));
     }
 }
