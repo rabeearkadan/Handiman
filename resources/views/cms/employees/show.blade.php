3e9c7ffb32e736652d706c38d74959ad50de5177
mohammad <itani0369-@hotmail.com>
2020-04-05 00:47:23 +0300

    Update show.blade.php

--- resources/views/cms/employees/show.blade.php
+++ resources/views/cms/employees/show.blade.php
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="icon">
-                    <i class="ion name"></i>
+                    <i class="ion ion-android-contact"></i>
                 </div>
 
             </div>
@@ -57,80 +57,98 @@
 
 
     <div class="row">
-<div class="col-lg-3">
-        <div class="card card-primary card-outline">
-            <div class="card-body box-profile">
-<div class="text-center">
-    <div style="border-radius: 50%; height: 180px; width: 180px; margin-left: auto; margin-right: auto; border: 3px solid #adb5bd">
+        <div class="col-lg-3">
+            <div class="card card-primary card-outline">
+                <div class="card-body box-profile">
+                    <div class="text-center">
+                        <div
+                            style="border-radius: 50%; height: 180px; width: 180px; margin-left: auto; margin-right: auto; border: 3px solid #adb5bd">
 
-        <div
-            style="border-radius: 50%; height: 174px; width: 174px; background-size: cover; background-position: center center; border: 3px solid #fff; background-image: url('https://mis.bau.edu.lb/web/v12/iConnectV12/admin/ProfileImage.aspx?ID=201801949&Code=F0DC386DD0')">
+                            <div
+                                style="border-radius: 50%; height: 174px; width: 174px; background-size: cover; background-position: center center; border: 3px solid #fff; background-image: url('https://mis.bau.edu.lb/web/v12/iConnectV12/admin/ProfileImage.aspx?ID=201801949&Code=F0DC386DD0')">
 
-        </div>
+                            </div>
+                            <h3 class="profile-username text-center">
 
-    </div>
-</div>
+                                <span> {{$employee->name}}</span>
+                            </h3>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
     </div>
+
 @endsection
 
 
 @push('css')
     <style>
-        @media (min-width: 1200px){
+        @media (min-width: 1200px) {
             .col-lg-3 .small-box h3, .col-md-3 .small-box h3, .col-xl-3 .small-box h3 {
-                 font-size: 2.2rem;
-            }}
+                font-size: 2.2rem;
+            }
+        }
 
-            @media (min-width: 992px){
-                .col-lg-3 .small-box h3, .col-md-3 .small-box h3, .col-xl-3 .small-box h3 {
-                     font-size: 1.6rem;
-                }}
-                .small-box h3, .small-box p {
-                     z-index: 5;
-                }
-                .small-box h3 {
-                     font-size: 2.2rem;
-                     font-weight: 700;
-                     margin: 0 0 10px 0;
-                     padding: 0;
-                     white-space: nowrap;
-                }
-                .h3, h3 {
-                    font-size: 1.75rem;
-                }
-                .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
-                    margin-bottom: .5rem;
-                    font-family: inherit;
-                    font-weight: 500;
-                    line-height: 1.2;
-                    color: inherit;
-                }
-                h1, h2, h3, h4, h5, h6 {
-                    margin-top: 0;
-                    margin-bottom: .5rem;
-                }
-                *, ::after, ::before {
-                    box-sizing: border-box;
-                }
-                h3 {
-                    display: block;
-                    font-size: 1.17em;
-                    margin-block-start: 1em;
-                    margin-block-end: 1em;
-                    margin-inline-start: 0px;
-                    margin-inline-end: 0px;
-                    font-weight: bold;
-                }
+        @media (min-width: 992px) {
+            .col-lg-3 .small-box h3, .col-md-3 .small-box h3, .col-xl-3 .small-box h3 {
+                font-size: 1.6rem;
+            }
+        }
+
+        .small-box h3, .small-box p {
+            z-index: 5;
+        }
+
+        .small-box h3 {
+            font-size: 2.2rem;
+            font-weight: 700;
+            margin: 0 0 10px 0;
+            padding: 0;
+            white-space: nowrap;
+        }
+
+        .h3, h3 {
+            font-size: 1.75rem;
+        }
+
+        .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
+            margin-bottom: .5rem;
+            font-family: inherit;
+            font-weight: 500;
+            line-height: 1.2;
+            color: inherit;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            margin-top: 0;
+            margin-bottom: .5rem;
+        }
+
+        *, ::after, ::before {
+            box-sizing: border-box;
+        }
+
+        h3 {
+            display: block;
+            font-size: 1.17em;
+            margin-block-start: 1em;
+            margin-block-end: 1em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
+            font-weight: bold;
+        }
+
         .card-primary.card-outline {
             border-top: 3px solid #007bff;
         }
+
         .card {
-            box-shadow: 0 0 1px rgba(0,0,0,.125), 0 1px 3px rgba(0,0,0,.2);
+            box-shadow: 0 0 1px rgba(0, 0, 0, .125), 0 1px 3px rgba(0, 0, 0, .2);
             margin-bottom: 1rem;
         }
+
         .card {
             position: relative;
             display: -ms-flexbox;
@@ -141,12 +159,14 @@
             word-wrap: break-word;
             background-color: #fff;
             background-clip: border-box;
-            border: 0 solid rgba(0,0,0,.125);
+            border: 0 solid rgba(0, 0, 0, .125);
             border-radius: .25rem;
         }
+
         *, ::after, ::before {
             box-sizing: border-box;
         }
+
         h3 {
             display: block;
             font-size: 1.17em;
@@ -181,6 +201,11 @@
 
         *, ::after, ::before {
             box-sizing: border-box;
+        }
+
+        .profile-username {
+            font-size: 21px;
+            margin-top: 5px;
         }
 
         p {
