a172fa8a10f58eb7074ae96106ea89da04a94311
mohammad <itani0369-@hotmail.com>
2020-04-04 05:04:52 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -43,18 +43,10 @@ Route::middleware(['auth:api', 'employee'])->prefix('employee')->group(function 
 Route::middleware(['auth:api', 'client'])->prefix('client')->group(function () {
 // api route with auth and client middleware the route is starts with api/client
     Route::post('request/{id}', 'RequestController@requestHandyman');
-
-
     Route::post('request-any', 'RequestController@requestAny');
     Route::get('post', 'PostController@getPosts');
     Route::post('make-request', 'RequestController@makeRequest');
-    //Route::post('send-message/{id}','RequestController@sendRequestMessage');
-    //  Route::get('load-message','RequestController@getMessages');
-
-
     Route::get('ongoing-requests', 'RequestController@getOngoingRequests');
-    // Route::get('outgoing-requests','RequestController@getOutgoingRequests');
-    // Route::put('accept-request',Request)
 
 });
 
@@ -66,7 +58,8 @@ Route::middleware(['auth:api'])->group(function () {
     Route::get('profile-edit', 'UserController@getProfile');
     Route::post('profile-edit', 'UserController@editProfile');
     Route::get('timeline-view/{id}', 'UserController@getTimeline');
-    Route::post('request-chat', 'ChatController@requestToChat');
+    Route::post('message', 'ChatController@sendMessage');
+    Route::get('message', 'ChatController@loadMessages');
 
     Route::post('credit-card', 'PaymentController@setCreditCard');
     Route::get('credit-card', 'PaymentController@getCreditCard');
