124415e759933e86bf62d043066cfa76a8e386ff
mohammad <itani0369-@hotmail.com>
2020-04-05 22:55:17 +0300

    Update SchedularEngine.php

--- app/Console/Commands/SchedularEngine.php
+++ app/Console/Commands/SchedularEngine.php
@@ -9,7 +9,6 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 
-
 class SchedularEngine extends Command
 {
     protected $signature = 'app:scheduler';
@@ -29,10 +28,14 @@ class SchedularEngine extends Command
         foreach ($request as $req) {
             $handyman = $this->searchForHandyman($req);
             if ($handyman == null) {
+                $user = User::query()->find($req->client_ids[0]);
+                $this->Notification($user->cient_device_token, 'Admin', 'You have less than 10 minutes to reply for pending requests', 'notification');
 
             } else {
-                $req->employee;
                 $req->save();
+                $req->employees()->attach($handyman->id);
+                $this->Notification($handyman->employee_device_token, 'Admin', 'You received a new request', 'request');
+
             }
 
         }
@@ -43,7 +46,7 @@ class SchedularEngine extends Command
     private function searchForHandyman($requestHandyman)
     {
 
-        $day = Carbon::now()->dayOfWeek;
+
         $availableUsers = User::query()
             ->where('service_ids', $requestHandyman->service_id)
             ->where('location', 'near', [
@@ -59,9 +62,26 @@ class SchedularEngine extends Command
             ])->orderBy('dist.calculated')->get();
 
         $matchingHandyman = null;
+        if (Carbon::now($requestHandyman->timezone)->minute > 30) {
+            $nowHour = str_pad(Carbon::now($requestHandyman->timezone)->hour + 1, 2, '0', STR_PAD_LEFT) . '00';
+            $nowNextHour = str_pad(Carbon::now($requestHandyman->timezone)->hour + 2, 2, '0', STR_PAD_LEFT) . '00';
+        } else {
+            $nowHour = str_pad(Carbon::now($requestHandyman->timezone)->hour, 2, '0', STR_PAD_LEFT) . '00';
+            $nowNextHour = str_pad(Carbon::now($requestHandyman->timezone)->hour + 1, 2, '0', STR_PAD_LEFT) . '00';
+        }
+
         foreach ($availableUsers as $handyman) {
-            $flag1 = $this->checkTimeline($from, $to, $day, $handyman);
-            $flag2 = $this->checkRequests($handyman, $day, $from, $to);
+            if ($requestHandyman->from == null) {
+                $requestHandyman->from = $nowHour;
+            }
+            if ($requestHandyman->to == null) {
+                $requestHandyman->to = $nowNextHour;
+            }
+            if ($requestHandyman->day == null) {
+                $requestHandyman->day = $day = Carbon::now()->dayOfWeek;
+            }
+            $flag1 = $this->checkTimeline($requestHandyman->from, $requestHandyman->to, $requestHandyman->day, $handyman);
+            $flag2 = $this->checkRequests($handyman, $requestHandyman->day, $requestHandyman->from, $requestHandyman->to);
             if ($flag1 && $flag2) {
                 $matchingHandyman = $handyman;
                 break;
@@ -86,13 +106,13 @@ class SchedularEngine extends Command
 
         return response()->json(['status' => 'success', 'notification' => $notification]);
     }
+
     public function checkTimeline($from, $to, $day, User $handyman)
     {
         $flag = true;
-        for ($i = $from; $i <= $to; $i++) {
-            $hour = str_pad($i,
-                    2, 0, STR_PAD_LEFT) . "00";
-            if ($handyman->timeline[$day][$i] == true) {
+        for ($i = (int)$from; $i <= (int)$to; $i = $i + 100) {
+            $hour = str_pad($i, 4, "0", STR_PAD_LEFT);
+            if ($handyman->timeline[$day][$hour] == false) {
                 $flag = false;
                 break;
             }
@@ -105,8 +125,6 @@ class SchedularEngine extends Command
         $requestsq = RequestService::query()->whereHas('employees', function ($q) use ($handyman) {
             $q->where('_id', $handyman->_id);
         })->where('day', $day)->where('status', '!=', 'done');
-        //0800 or 08:00
-        //0800 -> 1000
         for ($i = (int)$from; $i <= (int)$to; $i = $i + 100) {
             $requestsq->where('from', str_pad($i, 4, "0", STR_PAD_LEFT));
         }
