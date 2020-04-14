84a40473c136a53faf49ec07716cc73bd243b6db
mohammad <itani0369-@hotmail.com>
2020-04-14 10:13:11 +0300

    Update RequestService.php

--- app/Models/RequestService.php
+++ app/Models/RequestService.php
@@ -10,10 +10,7 @@ class RequestService extends Eloquent
 {
     //
     protected $table = 'requests';
-    protected $casts = [
-        'date' => 'datetime:YYYY-MM-DD',
-    ];
-
+   
     public function users()
     {
         return $this->belongsToMany(User::class);
