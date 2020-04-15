86173a3b439bac4c1705708c7d4bc964e794dcc0
mohammad <itani0369-@hotmail.com>
2020-04-15 05:37:03 +0300

    Update cms.php

--- routes/cms.php
+++ routes/cms.php
@@ -7,6 +7,7 @@ Route::get("/home", 'HomeController@index')->name('admin.home');
 Route::resource('service', 'ServiceController');
 Route::resource('client', 'ClientController');
 Route::resource('employee', 'HandymanController');
+Route::resource('statistics', 'StatisticsController');
 Route::get('google-piechart', array('as' => 'chart.piechart', 'uses' => 'StatisticsController@pieChart'));
 
 Route::get('service.test', ['uses' => 'ServiceController@test', 'as' => 'service.test']);
