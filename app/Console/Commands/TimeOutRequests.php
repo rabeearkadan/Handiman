7d57d803809f78e8d04ed12ece3a97e2ad71cc5d
mohammad <itani0369-@hotmail.com>
2020-04-22 23:09:26 +0300

    Update TimeOutRequests.php

--- app/Console/Commands/TimeOutRequests.php
+++ app/Console/Commands/TimeOutRequests.php
@@ -45,23 +45,19 @@ class TimeOutRequests extends Command
         $requests = RequestService::query()->where('status', 'pending')->get();
 
         foreach ($requests as $req) {
-            $handyman = User::query()->find($req->employee_ids[0]);
-            $handyman_device = $handyman->employee_device_token;
-            $nowTime = Carbon::now();
-            $duration = $nowTime->diffInMinutes($req->updated_at);
-           // $this->Notification($handyman_device, 'Admin', $duration, 'notification');
+            $nowTime = Carbon::now($req->timezone);
             if ($req->employees()->count() > 0) {
                 $client = User::query()->find($req->client_ids[0]);
-                $client_device = $client->client_device_token;
                 $handyman = User::query()->find($req->employee_ids[0]);
+
+                $client_device = $client->client_device_token;
                 $handyman_device = $handyman->employee_device_token;
                 $duration = $nowTime->diffInMinutes($req->updated_at);
                 if ($duration > 30) {
-//                    $req->employee_ids=null;
-//                    $req->save();
-
+                    $req->employees()->attach(null);
+                    $req->save();
                     $this->Notification($client_device, 'Admin', "Handyman didn't respond, your request will be handled by the system", 'notification');
-                } else if ($duration >= 20 && $duration<=30) {
+                } else if ($duration >= 20 && $duration <= 30) {
                     $this->Notification($handyman_device, 'Admin', 'You have less than 10 minutes to reply for pending requests', 'notification');
 
                 }
