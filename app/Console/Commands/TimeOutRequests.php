10342cb83f5c18849783331ce829cd87f951549c
mohammad <itani0369-@hotmail.com>
2020-04-25 02:29:56 +0300

    Update TimeOutRequests.php

--- app/Console/Commands/TimeOutRequests.php
+++ app/Console/Commands/TimeOutRequests.php
@@ -55,9 +55,9 @@ class TimeOutRequests extends Command
                 $duration = $nowTime->diffInMinutes($req->updated_at);
                 if ($duration > 30) {
                     $this->Notification($client_device, 'Admin', "Handyman didn't respond, your request will be handled by the system", 'notification');
+                    $req->employees()->detach();
 
-                    
-                    } else if ($duration >= 20 && $duration <= 30) {
+                } else if ($duration >= 20 && $duration <= 30) {
                     $this->Notification($handyman_device, 'Admin', 'You have less than 10 minutes to reply for pending requests', 'notification');
 
                 }
