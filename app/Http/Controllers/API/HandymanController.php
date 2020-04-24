35a2b25f8925fcc0b9693a76c647f29531a057a7
mohammad <itani0369-@hotmail.com>
2020-04-25 01:59:18 +0300

    Update HandymanController.php

--- app/Http/Controllers/API/HandymanController.php
+++ app/Http/Controllers/API/HandymanController.php
@@ -80,7 +80,7 @@ class HandymanController extends Controller
                     'distanceField' => "dist.calculated",
                     '$maxDistance' => 500000,
                 ],
-            ])->orderBy('dist.calculated')->get();
+            ])->orderBy('dist.calculated');
         return response()->json(['status' => 'success', 'HandymanList' => $handymanList]);
     }
 
