022cd0a9f6aa1304d7ece3fdc37da9b0b0a78545
mohammad <itani0369-@hotmail.com>
2020-05-06 23:43:23 +0300

    Update RequestController.php

--- app/Http/Controllers/API/RequestController.php
+++ app/Http/Controllers/API/RequestController.php
@@ -9,6 +9,7 @@ use  App\Models\RequestService;
 
 use App\Models\Service;
 use App\User;
+use Barryvdh\DomPDF\PDF;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
