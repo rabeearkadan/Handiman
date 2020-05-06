c38a7c9a548ea6875adf103e7c8b3012c1762534
mohammad <itani0369-@hotmail.com>
2020-05-06 03:31:47 +0300

    Update RequestController.php

--- app/Http/Controllers/API/RequestController.php
+++ app/Http/Controllers/API/RequestController.php
@@ -168,7 +168,7 @@ class RequestController extends Controller
                 'description' => $user->name,
                 'source' => $token,
                 'capture' => true,
-                'receipt_email'=>''
+                'receipt_email'=>'itani0369-@hotmail.com'
 
             ]);
 
