defbd74be4f41a3d43a046844141b1b0f64116ad
mohammad <itani0369-@hotmail.com>
2020-05-08 03:37:18 +0300

    .

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Mail\contact;
+use App\Models\ContactedUs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
     //
-    public function contact(Request $request){
+    public function contact(Request $request)
+    {
+        $msg = new ContactedUs();
+        $msg->subject = $request->input('subject');
+        $msg->message = $request->input('message');
+        $msg->email = $request->input('email');
+        $msg->from = $request->input('name');
+        $msg->save();
         $request->validate(["email" => 'email']);
         Mail::to($request->email)->send(new contact());
         return 'OK';
     }
+
 }
---
+++ app/Models/ContactedUs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class ContactedUs extends Eloquent
+{
+    protected $table='contacted_us';
+
+}
