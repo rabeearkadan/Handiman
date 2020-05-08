d4faf8b65ea30bdf6ea0f253d07ae5343443a35f
mohammad <itani0369-@hotmail.com>
2020-05-08 04:22:26 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -155,7 +155,7 @@ About Section
                     becomes easy for both the parties. An individual can get the right person for a reasonable cost and
                     the handy service provider can get connected with the customers easily.
                 </p>
-                
+
             </div>
         </div>
     </div>
@@ -170,8 +170,7 @@ Services Section
             <div class="col-md-12">
                 <h3 class="section-title">Our Services</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                    praesentium</p>
+
             </div>
         </div>
 
@@ -234,96 +233,7 @@ Subscrbe Section
     </div>
 </section>
 
-<!--==========================
-Porfolio Section
-============================-->
-<section id="portfolio">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="section-title">Portfolio</h3>
-                <div class="section-title-divider"></div>
-                <p class="section-description">Si stante, hoc natura videlicet vult, salvam esse se, quod concedimus ses
-                    haec dicturum fuisse</p>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-1.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 1</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-2.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 2</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-3.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 3</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-4.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 4</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-5.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 5</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-6.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 6</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-7.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 7</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(../../public/img/portfolio-8.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 8</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-        </div>
-    </div>
-</section>
 
 <!--==========================
 Testimonials Section
