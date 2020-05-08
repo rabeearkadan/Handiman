75ef5990a9cfc0695aaf3b1673a04af2feb4951e
mohammad <itani0369-@hotmail.com>
2020-05-08 04:29:35 +0300

    Update header.blade.php

--- resources/views/cms/layouts/header.blade.php
+++ resources/views/cms/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <div class="app-header header-shadow">
     <div class="app-header__logo">
-        <div class="logo-src"></div>
+        <img src="../../../../public/img/logo.png">
         <div class="header__pane ml-auto">
             <div>
                 <button type="button" class="hamburger close-sidebar-btn hamburger--elastic"
