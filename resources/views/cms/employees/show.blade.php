4f8028025a1db477d64fae3f669bff9dfbaa1e60
mohammad <itani0369-@hotmail.com>
2020-05-11 23:36:16 +0300

    Update show.blade.php

--- resources/views/cms/employees/show.blade.php
+++ resources/views/cms/employees/show.blade.php
@@ -21,7 +21,7 @@
             </div>
         </div>
 
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-5 col-6">
 
             <div class="small-box bg-info">
                 <div class="inner">
