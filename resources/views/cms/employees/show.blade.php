f9f4621ad421dcc697913f2000fff59b5fa31c94
mohammad <itani0369-@hotmail.com>
2020-05-12 00:30:11 +0300

    Update show.blade.php

--- resources/views/cms/employees/show.blade.php
+++ resources/views/cms/employees/show.blade.php
@@ -202,90 +202,90 @@
                                 </button>
                             </td>
                         </tr>
-                        <tr>
-                            <td class="text-center text-muted">#347</td>
-                            <td>
-                                <div class="widget-content p-0">
-                                    <div class="widget-content-wrapper">
-                                        <div class="widget-content-left mr-3">
-                                            <div class="widget-content-left">
-                                                <img width="40" class="rounded-circle"
-                                                     src="assets/images/avatars/3.jpg" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="widget-content-left flex2">
-                                            <div class="widget-heading">Ruben Tillman</div>
-                                            <div class="widget-subheading opacity-7">Etiam sit amet orci eget
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-center">Berlin</td>
-                            <td class="text-center">
-                                <div class="badge badge-success">Completed</div>
-                            </td>
-                            <td class="text-center">
-                                <button type="button" id="PopoverCustomT-2" class="btn btn-primary btn-sm">
-                                    Details
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-center text-muted">#321</td>
-                            <td>
-                                <div class="widget-content p-0">
-                                    <div class="widget-content-wrapper">
-                                        <div class="widget-content-left mr-3">
-                                            <div class="widget-content-left">
-                                                <img width="40" class="rounded-circle"
-                                                     src="assets/images/avatars/2.jpg" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="widget-content-left flex2">
-                                            <div class="widget-heading">Elliot Huber</div>
-                                            <div class="widget-subheading opacity-7">Lorem ipsum dolor sic</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-center">London</td>
-                            <td class="text-center">
-                                <div class="badge badge-danger">In Progress</div>
-                            </td>
-                            <td class="text-center">
-                                <button type="button" id="PopoverCustomT-3" class="btn btn-primary btn-sm">
-                                    Details
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-center text-muted">#55</td>
-                            <td>
-                                <div class="widget-content p-0">
-                                    <div class="widget-content-wrapper">
-                                        <div class="widget-content-left mr-3">
-                                            <div class="widget-content-left">
-                                                <img width="40" class="rounded-circle"
-                                                     src="assets/images/avatars/1.jpg" alt=""></div>
-                                        </div>
-                                        <div class="widget-content-left flex2">
-                                            <div class="widget-heading">Vinnie Wagstaff</div>
-                                            <div class="widget-subheading opacity-7">UI Designer</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-center">Amsterdam</td>
-                            <td class="text-center">
-                                <div class="badge badge-info">On Hold</div>
-                            </td>
-                            <td class="text-center">
-                                <button type="button" id="PopoverCustomT-4" class="btn btn-primary btn-sm">
-                                    Details
-                                </button>
-                            </td>
-                        </tr>
+{{--                        <tr>--}}
+{{--                            <td class="text-center text-muted">#347</td>--}}
+{{--                            <td>--}}
+{{--                                <div class="widget-content p-0">--}}
+{{--                                    <div class="widget-content-wrapper">--}}
+{{--                                        <div class="widget-content-left mr-3">--}}
+{{--                                            <div class="widget-content-left">--}}
+{{--                                                <img width="40" class="rounded-circle"--}}
+{{--                                                     src="assets/images/avatars/3.jpg" alt="">--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="widget-content-left flex2">--}}
+{{--                                            <div class="widget-heading">Ruben Tillman</div>--}}
+{{--                                            <div class="widget-subheading opacity-7">Etiam sit amet orci eget--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </td>--}}
+{{--                            <td class="text-center">Berlin</td>--}}
+{{--                            <td class="text-center">--}}
+{{--                                <div class="badge badge-success">Completed</div>--}}
+{{--                            </td>--}}
+{{--                            <td class="text-center">--}}
+{{--                                <button type="button" id="PopoverCustomT-2" class="btn btn-primary btn-sm">--}}
+{{--                                    Details--}}
+{{--                                </button>--}}
+{{--                            </td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <td class="text-center text-muted">#321</td>--}}
+{{--                            <td>--}}
+{{--                                <div class="widget-content p-0">--}}
+{{--                                    <div class="widget-content-wrapper">--}}
+{{--                                        <div class="widget-content-left mr-3">--}}
+{{--                                            <div class="widget-content-left">--}}
+{{--                                                <img width="40" class="rounded-circle"--}}
+{{--                                                     src="assets/images/avatars/2.jpg" alt="">--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="widget-content-left flex2">--}}
+{{--                                            <div class="widget-heading">Elliot Huber</div>--}}
+{{--                                            <div class="widget-subheading opacity-7">Lorem ipsum dolor sic</div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </td>--}}
+{{--                            <td class="text-center">London</td>--}}
+{{--                            <td class="text-center">--}}
+{{--                                <div class="badge badge-danger">In Progress</div>--}}
+{{--                            </td>--}}
+{{--                            <td class="text-center">--}}
+{{--                                <button type="button" id="PopoverCustomT-3" class="btn btn-primary btn-sm">--}}
+{{--                                    Details--}}
+{{--                                </button>--}}
+{{--                            </td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <td class="text-center text-muted">#55</td>--}}
+{{--                            <td>--}}
+{{--                                <div class="widget-content p-0">--}}
+{{--                                    <div class="widget-content-wrapper">--}}
+{{--                                        <div class="widget-content-left mr-3">--}}
+{{--                                            <div class="widget-content-left">--}}
+{{--                                                <img width="40" class="rounded-circle"--}}
+{{--                                                     src="assets/images/avatars/1.jpg" alt=""></div>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="widget-content-left flex2">--}}
+{{--                                            <div class="widget-heading">Vinnie Wagstaff</div>--}}
+{{--                                            <div class="widget-subheading opacity-7">UI Designer</div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </td>--}}
+{{--                            <td class="text-center">Amsterdam</td>--}}
+{{--                            <td class="text-center">--}}
+{{--                                <div class="badge badge-info">On Hold</div>--}}
+{{--                            </td>--}}
+{{--                            <td class="text-center">--}}
+{{--                                <button type="button" id="PopoverCustomT-4" class="btn btn-primary btn-sm">--}}
+{{--                                    Details--}}
+{{--                                </button>--}}
+{{--                            </td>--}}
+{{--                        </tr>--}}
                             @endforeach
                         </tbody>
                     </table>
