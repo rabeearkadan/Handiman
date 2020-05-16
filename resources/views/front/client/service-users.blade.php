6646e838da84c18c5599192a70a798ba7f3a14d2
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-16 18:54:35 +0300

    no message

--- resources/views/front/client/service-users.blade.php
+++ resources/views/front/client/service-users.blade.php
@@ -111,6 +111,9 @@
                                                     <i class="fa fa-star"></i>
                                                     @endif
                                                 @endfor
+                                                    @for($x=5-$employee->rating;$x>0;$x--)
+                                                            <i class="fa fa-star-o"></i>
+                                                    @endfor
                                             </div><!-- /.card-row-rating -->
                                         </dt>
                                     </dl>
