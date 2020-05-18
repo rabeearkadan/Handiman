e68bd8b519093312048f073dbbc828b338ec0e7f
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-18 04:05:01 +0300

    no message

--- app/Http/Controllers/FRONT/Client/ProfileController.php
+++ app/Http/Controllers/FRONT/Client/ProfileController.php
@@ -47,6 +47,7 @@ class ProfileController extends Controller
         ];
         $user->push('client_addresses',$data);
         $user->save();
+        dd($user);
         return view('front.client.profile.edit-profile', compact('user'));
     }
 
