597ffc03348216954f7226cea5bf465b2967bce7
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-20 22:31:46 +0300

    no message

--- resources/views/front/employee/profile/schedule.blade.php
+++ resources/views/front/employee/profile/schedule.blade.php
@@ -27,7 +27,7 @@
             <div class="form-horizontal">
                 <div class="form-group">
                     <label for="slider{{$day}}" class="col-sm-1 control-label"> {{date('D', strtotime("Sunday +{$day} days"))}}</label>
-                    <div class="col-sm-11" style="margin-top: 12px;">
+                    <div class="col-sm-11" style="margin-top: 12px;margin-bottom: 20px;">
                         <div class="col-sm-12">
                         <div id="slider{{$day}}"></div>
                         <input type="hidden" id="periods{{$day}}" name="periods{{$day}}" value="">
@@ -35,8 +35,8 @@
                         <div class="col-sm-12">
                             <div class="ticks">
                             @for($hour=0;$hour<=24;$hour++)
-                                    <span class="slider-tick-mark-main" style="left:{{$hour*3.9875+2.2}}%;"></span>
-                                    <span class="slider-tick-mark-main-text" style="left:{{$hour*4.16666666}}%;">{{$hour}}</span>
+                                    <span class="slider-tick-mark-main" style="left:{{$hour*3.87+2}}%;"></span>
+                                    <span class="slider-tick-mark-main-text" style="left:{{$hour*3.87+2}}%;">{{$hour}}</span>
                                 @endfor
                             </div><!-- /.ticks-* -->
                         </div><!-- /.col-* -->
