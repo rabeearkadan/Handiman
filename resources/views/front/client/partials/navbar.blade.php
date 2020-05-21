3fd763dfa5deefaef8c394f2941b9b0b38d68e41
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-22 01:39:24 +0300

    no message

--- resources/views/front/client/partials/navbar.blade.php
+++ resources/views/front/client/partials/navbar.blade.php
@@ -1,4 +1,5 @@
 <nav class="nav">
+    <a href="#" data-target="slide-out" class="nav-item"><i class="fa fa-bars">menu</i></a>
     <a href="{{route('client.home')}}" class="nav-item @if (  request()->is('client/home*')) {{'is-active'}} @endif" active-color="green">Home</a>
     <a href="{{route('client.service')}}"  class="nav-item @if ( request()->is('client/services*')) {{'is-active'}} @endif" active-color="orange">Services</a>
     <a href="{{route('client.request.index')}}" class="nav-item  @if ( request()->is('client/request*')) {{'is-active'}} @endif " active-color="blue">Requests</a>
