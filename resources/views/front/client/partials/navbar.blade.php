fbfd7b917ba98666774064ee067bad9a954952fe
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-22 02:15:40 +0300

    no message

--- resources/views/front/client/partials/navbar.blade.php
+++ resources/views/front/client/partials/navbar.blade.php
@@ -31,11 +31,7 @@
         const indicator = document.querySelector('.nav-indicator');
         const items = document.querySelectorAll('.nav-item');
         function handleIndicator(el) {
-            
 
-            indicator.style.width = `${el.offsetWidth}px`;
-            indicator.style.left = `${el.offsetLeft}px`;
-            indicator.style.backgroundColor = el.getAttribute('active-color');
 
             el.classList.add('is-active');
             el.style.color = el.getAttribute('active-color');
