382467d066b785d0d9f906ab4346c0ee76536d99
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-22 00:17:51 +0300

    no message

--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -22,6 +22,28 @@
 
 @include('front.client.partials.preloader')
 @include('front.client.partials.navbar')
+
+<ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+            <div class="background">
+                <img src="">
+            </div>
+            <a href="#user"><img class="circle" src=""></a>
+            <a href="#name"><span class="black-text name">nnnn</span></a>
+            <a href="#email"><span class="black-text email">nnnnn@gmail.com</span></a>
+        </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+</ul>
+<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+
+
+
+
 <div class="content">
     @yield('content')
 </div>
@@ -33,6 +55,7 @@
         $('.sidenav').sidenav();
     });
 </script>
+
 @stack('js')
 
 </body>
