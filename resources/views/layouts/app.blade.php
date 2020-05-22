d31ea6f139a87bd71f245ebf9214aa4ce886dda7
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 00:43:16 +0300

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,49 +7,23 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Styles -->
-
-    <link href="{{asset('css/materialize.min.css')}}" rel="stylesheet">
-
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     @stack('css')
 
 </head>
 <body>
-<nav class="black">
-    <div class="nav-wrapper">
-        <a href="#" class="brand-logo">lll</a>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul class="right hide-on-small-only">
-            <li><a href="{{route('register')}}">Register</a></li>
-            <li><a href="{{url('/#about')}}">Abouts us</a></li>
-            <li><a href="">Our Team</a></li>
-            <li><a href="">Contact us</a></li>
-        </ul>
-    </div>
-</nav>
-
-<ul class="sidenav" id="mobile-demo">
-    <li><a href="{{route('register')}}">Register</a></li>
-    <li><a href="{{url('/#about')}}">Abouts us</a></li>
-    <li><a href="{{url('/#team')}}">Our Team</a></li>
-    <li><a href="{{url('/#contact')}}">Contact us</a></li>
-</ul>
     <div id="app">
+        <div id="preloader"></div>
         @yield('content')
     </div>
-
 @stack('script')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="/public/js/materialize.js" type="text/javascript"></script>
-    <script>
-    $(document).ready(function(){
-        $('.sidenav').sidenav();
-    });
-</script>
+
 </body>
 </html>
