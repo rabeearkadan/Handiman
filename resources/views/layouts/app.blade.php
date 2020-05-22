16d38e1c2e710a0262bc6647d0e85a37e26bfbb0
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 00:26:06 +0300

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
     <!-- Scripts -->
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -20,20 +19,7 @@
     <link href="{{asset('css/footer.css')}}" rel="stylesheet">
     <link href="{{asset('css/social-buttons.css')}}" rel="stylesheet">
     <style>
-        .footer{
-            position:absolute;
-            bottom:0;
-            width:100%;
-        }
-        body {
-            display: flex;
-            min-height: 100vh;
-            flex-direction: column;
-        }
 
-        main {
-            flex: 1 0 auto;
-        }
     </style>
     @stack('css')
 
@@ -59,57 +45,9 @@
     <li><a href="{{url('/#contact')}}">Contact us</a></li>
 </ul>
     <div id="app">
-        <div id="preloader"></div>
         @yield('content')
     </div>
-    <footer class="footer">
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <h2>About Handiman.club</h2>
 
-                        <p>A Service-based platform to connect Handymen with their clients providing Smart Scheduling, easy payments...  </p>
-                    </div><!-- /.col-* -->
-
-                    <div class="col-sm-4">
-                        <h2>Contact Information</h2>
-
-                        <p>
-                            Street 123, Beirut, Lebanon<br>
-                            +961-71-123-456-, <a href="#">handiman@gmail.com</a>
-                        </p>
-                    </div><!-- /.col-* -->
-
-                    <div class="col-sm-4">
-                        <h2>Stay Connected</h2>
-
-                        <ul class="social-links nav nav-pills">
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                        </ul><!-- /.header-nav-social -->
-                    </div><!-- /.col-* -->
-                </div><!-- /.row -->
-            </div><!-- /.container -->
-        </div><!-- /.footer-top -->
-
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="footer-bottom-left">
-                    © 2020 All rights reserved.
-                </div><!-- /.footer-bottom-left -->
-
-                <div class="footer-bottom-right">
-                    <ul class="nav nav-pills">
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Terms &amp; Conditions</a></li>
-                        <li><a href="">Contact</a></li>
-                    </ul><!-- /.nav -->
-                </div><!-- /.footer-bottom-right -->
-            </div><!-- /.container -->
-        </div><!-- /.footer-bottom -->
-    </footer><!-- /.footer -->
 @stack('script')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="/public/js/materialize.js" type="text/javascript"></script>
