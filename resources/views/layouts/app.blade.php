df11875f3ad42175d6788bc3b710cefdb583f054
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 00:46:09 +0300

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <link href="{{asset('css/materialize.css')}}" rel="stylesheet">
+    <link href="{{asset('css/materialize.min.css')}}" rel="stylesheet">
 
     @stack('css')
 
