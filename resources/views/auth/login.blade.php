5aeed655b2591f4228b2ea0a9539a69f163edda1
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-22 23:27:22 +0300

    no message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,16 +10,12 @@
                     <div class="card-body">
                         <form method="POST" action="/login">
                             @csrf
-
                             <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
                                 <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
+                                    <input id="email" type="email" class="validate  @error('email') is-invalid @enderror" name="email"
                                            value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                    <label for="email">{{ __('E-Mail Address') }}</label>
+                                    <span class="helper-text" data-error="wrong" data-success="right"></span>
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -29,14 +25,10 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
+                                <div class="input-field col m12">
+                                    <input id="password" type="password" class="validate @error('password') is-invalid @enderror" name="password"
                                            required autocomplete="current-password">
-
+                                    <label for="password">{{ __('Password') }}</label>
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,44 +12,115 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <link href="{{asset('css/navbar.css')}}" rel="stylesheet">
+{{--    <link href="{{asset('css/navbar.css')}}" rel="stylesheet">--}}
+{{--    Remember to delete the file --}}
+    <link href="{{asset('css/footer.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/social-buttons.css')}}" rel="stylesheet">
+    <link href="{{asset('css/materialize.min.css')}}" rel="stylesheet">
+    <style>
+        .footer{
+            position:absolute;
+            bottom:0;
+            width:100%;
+        }
+        body {
+            display: flex;
+            min-height: 100vh;
+            flex-direction: column;
+        }
+
+        main {
+            flex: 1 0 auto;
+        }
+    </style>
     @stack('css')
 
 </head>
 <body>
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <div class="navTrigger">
-            <i></i><i></i><i></i>
-        </div>
-    </button>
 
-
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-        </ul>
-    </div>
-</nav>
     <div id="app">
         <div id="preloader"></div>
+
+        <nav class="black">
+            <div class="nav-wrapper">
+                <a href="{{url('/')}}" class="brand-logo"><img src="/public/img/logo.png" alt="Home"></a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="{{route('register')}}">Register</a></li>
+                    <li><a href="{{url('/#about')}}">Abouts us</a></li>
+                    <li><a href="{{url('/#team')}}">Our Team</a></li>
+                    <li><a href="{{url('/#contact')}}">Contact us</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+            <li><a href="{{route('register')}}">Register</a></li>
+            <li><a href="{{url('/#about')}}">Abouts us</a></li>
+            <li><a href="{{url('/#team')}}">Our Team</a></li>
+            <li><a href="{{url('/#contact')}}">Contact us</a></li>
+        </ul>
         @yield('content')
     </div>
+    <footer class="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h2>About Handiman.club</h2>
 
+                        <p>A Service-based platform to connect Handymen with their clients providing Smart Scheduling, easy payments...  </p>
+                    </div><!-- /.col-* -->
+
+                    <div class="col-sm-4">
+                        <h2>Contact Information</h2>
+
+                        <p>
+                            Street 123, Beirut, Lebanon<br>
+                            +961-71-123-456-, <a href="#">handiman@gmail.com</a>
+                        </p>
+                    </div><!-- /.col-* -->
+
+                    <div class="col-sm-4">
+                        <h2>Stay Connected</h2>
+
+                        <ul class="social-links nav nav-pills">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        </ul><!-- /.header-nav-social -->
+                    </div><!-- /.col-* -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.footer-top -->
+
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="footer-bottom-left">
+                    © 2020 All rights reserved.
+                </div><!-- /.footer-bottom-left -->
+
+                <div class="footer-bottom-right">
+                    <ul class="nav nav-pills">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Terms &amp; Conditions</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul><!-- /.nav -->
+                </div><!-- /.footer-bottom-right -->
+            </div><!-- /.container -->
+        </div><!-- /.footer-bottom -->
+    </footer><!-- /.footer -->
 @stack('script')
-<script>
-        $('.navTrigger').click(function(){
-            $(this).toggleClass('active');
-        });
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="/public/js/materialize.js" type="text/javascript"></script>
+    <script>
+    $(document).ready(function(){
+        $('.sidenav').sidenav();
+    });
 </script>
 </body>
 </html>
