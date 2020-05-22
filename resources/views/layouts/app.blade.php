76b88550c8f2ca07b2dd6f4896b1771fb3e05baa
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 00:45:18 +0300

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,15 +15,47 @@
 
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link href="{{asset('css/materialize.css')}}" rel="stylesheet">
+
     @stack('css')
 
 </head>
 <body>
-    <div id="app">
+<nav class="black">
+    <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="">Register</a></li>
+            <li><a href="">Abouts us</a></li>
+            <li><a href="">Our Team</a></li>
+            <li><a href="">Contact us</a></li>
+        </ul>
+    </div>
+</nav>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><a href="">Register</a></li>
+    <li><a href="">Register</a></li>
+    <li><a href="">Register</a></li>
+    <li><a href="">Register</a></li>
+</ul>
+
+<div id="app">
         <div id="preloader"></div>
         @yield('content')
     </div>
 @stack('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+<script src="/public/js/materialize.js" type="text/javascript"></script>
+
+<script>
+    $(document).ready(function(){
+        $('.sidenav').sidenav();
+    });
+
+
+</script>
 </body>
 </html>
