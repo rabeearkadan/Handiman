a252e35a492f627d536525637339d76f6bc090c5
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 00:23:25 +0300

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
 <body>
 <nav class="black">
     <div class="nav-wrapper">
-        <a href="" class="brand-logo">lll</a>
+        <a href="#" class="brand-logo">lll</a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
             <li><a href="{{route('register')}}">Register</a></li>
