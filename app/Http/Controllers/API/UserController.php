870a9d09d84e1fa1d38a67561ef0226a9dfa117f
mohammad <itani0369-@hotmail.com>
2020-05-23 23:31:07 +0300

    .

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -23,7 +23,8 @@ class UserController extends Controller
         $handyman = User::query()->find($request->input('handyman'));
         $request_id = $request->input('request');
         if ($handyman->rating[$request_id] != null) {
-            $handyman->rating[$request_id]['rating']=2;
+
+          $handyman->rating[$request_id]['rating'][0]=2;
             $handyman->push('rating.'.$request_id.'.ratings', 3);
         } else {
             $handyman->push('rating', $request_id);
