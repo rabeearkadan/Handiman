1b8e729bf656bed83f5493c135804cb8eb511a3c
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 19:30:12 +0300

    no message

--- resources/views/front/client/partials/navbar.blade.php
+++ resources/views/front/client/partials/navbar.blade.php
@@ -8,7 +8,7 @@
 <ul id="slide-out" class="sidenav">
     <li><div class="user-view">
             <div class="background">
-                <img >
+                <img src="/public/images/client/sidenav-background.jpg">
             </div>
             <a href="#user"><img class="circle" src="@if(Auth::user()->image){{config('image.path').Auth::user()->image}}@else /public/images/employee/profile-image.png @endif" alt="User"></a>
             <a href="#name"><span class="black-text name">{{ Auth::user()->name }}</span></a>
