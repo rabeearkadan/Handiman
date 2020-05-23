32c303aafde28c5106b030f7a9e87be5e8cff5ba
mohammad <itani0369-@hotmail.com>
2020-05-23 23:33:51 +0300

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -24,8 +24,8 @@ class UserController extends Controller
         $request_id = $request->input('request');
         if ($handyman->rating[$request_id] != null) {
 
-          $handyman->rating[$request_id]['rating'][0]=2;
-            $handyman->push('rating.'.$request_id.'.ratings', 3);
+            $handyman->rating . $request_id . 'rating' . [0] = 2;
+            $handyman->push('rating.' . $request_id . '.ratings', 3);
         } else {
             $handyman->push('rating', $request_id);
         }
