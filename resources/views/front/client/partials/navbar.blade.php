946c11a7d2e78d94ba7b1aa82aa7ab574c3e4647
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-23 19:04:04 +0300

    no message

--- resources/views/front/client/partials/navbar.blade.php
+++ resources/views/front/client/partials/navbar.blade.php
@@ -10,7 +10,7 @@
             <div class="background">
                 <img >
             </div>
-            <a href="#user"><img class="circle" src="{{ Auth::user()->image }}"></a>
+            <a href="#user"><img class="circle" src="@if(Auth::user()->image){{config('image.path').Auth::user()->image}}@else /public/images/employee/profile-image.png @endif" alt="User"></a>
             <a href="#name"><span class="black-text name">{{ Auth::user()->name }}</span></a>
             <a href="#email"><span class="black-text email">{{ Auth::user()->email }}</span></a>
         </div></li>
