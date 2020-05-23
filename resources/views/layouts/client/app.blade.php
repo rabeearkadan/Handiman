321b618958b63d97cb3b69e5b7361b4f9413b8ea
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-24 02:29:13 +0300

    no message

--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -32,11 +32,12 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="/public/js/materialize.js"></script>
-@stack('js')
 <script>
     $(document).ready(function(){
         $('.sidenav').sidenav();
     });
 </script>
+@stack('js')
+
 </body>
 </html>
