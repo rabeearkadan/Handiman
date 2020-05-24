a22fa7d0f74ba4a214662d1999b574916deba9ab
mohammad <itani0369-@hotmail.com>
2020-05-24 04:15:04 +0300

    Update HandymanController.php

--- app/Http/Controllers/API/HandymanController.php
+++ app/Http/Controllers/API/HandymanController.php
@@ -61,19 +61,19 @@ class HandymanController extends Controller
         if ($list == null)
             return response()->json(['status' => 'error', 'message' => "no service found"]);
         $users = $list->users()->where('isApproved', true)->get();
-        
 
-        $n = sizeof($users);
 
-        for ($i = 0; $i < $n; $i++) {
-            for ($j = 0; $j < $n - $i - 1; $j++) {
-                if ((double)$users[$j]->rating_object[$id] < (double)$users[$j+1]->rating_object[$id]) {
-                    $t = $users[$j];
-                    $users[$j] = $users[$j + 1];
-                    $users[$j + 1] = $t;
-                }
-            }
-        }
+//        $n = sizeof($users);
+//
+//        for ($i = 0; $i < $n; $i++) {
+//            for ($j = 0; $j < $n - $i - 1; $j++) {
+//                if ((double)$users[$j]->rating_object[$id] < (double)$users[$j+1]->rating_object[$id]) {
+//                    $t = $users[$j];
+//                    $users[$j] = $users[$j + 1];
+//                    $users[$j + 1] = $t;
+//                }
+//            }
+//        }
 
 
         return response()->json(['status' => 'successful', 'handymen' => $users]);
