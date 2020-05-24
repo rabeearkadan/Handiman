e8f66d56f9a51e59d1191ad3c3a9eb0e925027d7
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-24 12:51:26 +0300

    no message

--- resources/views/front/client/home.blade.php
+++ resources/views/front/client/home.blade.php
@@ -75,6 +75,7 @@
     $('.carousel.carousel-slider').carousel({
         fullWidth: true
     });
+    autoplay();
     function autoplay() {
         $('.carousel').carousel('next');
         setTimeout(autoplay, 4500);
