2a9cec4dbe6ceaa88d6781eff2350c7593ecf582
mohammad <itani0369-@hotmail.com>
2020-05-25 01:02:36 +0300

    Update show.blade.php

--- resources/views/cms/requests/show.blade.php
+++ resources/views/cms/requests/show.blade.php
@@ -56,51 +56,21 @@
                         </div>
                     </div>
 
-                    {{--                    --}}{{--                    // here will go the images og the request--}}
-
-
-                    {{--                    <div class="container">--}}
-                    {{--                        <div class="mySlides">--}}
-                    {{--                            <div class="numbertext">1 / 6</div>--}}
-                    {{--                            <img src={{asset('images/avatars/1.jpg')}}   style="width:100%">--}}
-                    {{--                        </div>--}}
-                    {{--                        <div class="mySlides">--}}
-                    {{--                            <div class="numbertext">1 / 6</div>--}}
-                    {{--                            <img src={{asset('images/avatars/1.jpg)}}   style="width:100%">--}}
-                    {{--                        </div>--}}
-
-
-                    {{--                        <a class="prev" onclick="plusSlides(-1)">❮</a>--}}
-                    {{--                        <a class="next" onclick="plusSlides(1)">❯</a>--}}
-
-                    {{--                        <div class="caption-container">--}}
-                    {{--                            <p id="caption"></p>--}}
-                    {{--                        </div>--}}
-
-                    {{--                        <div class="row">--}}
-                    {{--                            <div class="column">--}}
-                    {{--                                <img class="demo cursor" src={{asset('images/avatars/1.jpg)}}  style="width:100%" onclick="currentSlide(1)" alt="The Woods">--}}
-                    {{--                            </div>--}}
-                    {{--                            <div class="column">--}}
-                    {{--                                <img class="demo cursor" src={{asset('images/avatars/1.jpg)}}  style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">--}}
-                    {{--                            </div>--}}
-                    {{--                    </div>--}}
-                    {{--                    </div>--}}
 
                     <div class="container mt-2" id="services">
                         <div class="row">
-                            @foreach($request->images as $image)
+                            @for($i=0;$i<2;$i++)
                                 @if ( $loop->index % 4 == 0 )
                         </div>
                         <div class="row">
                             @endif
                             <div class="col-md-3 col-sm-6">
                                 <div class="card card-block">
-                                    <img src="{{config('image.path').$image}}" alt="later">
+                                    <img src="{{config('image.path').$request->images[$i]}}" alt="later">
                                     </a>
                                 </div>
                             </div>
-                            @endforeach
+                            @endfor
                         </div>
                     </div>
 
