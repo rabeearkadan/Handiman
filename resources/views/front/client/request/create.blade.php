7612dd76e683e422c40d1b4010dde17c542089f3
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-25 12:51:02 +0300

    no message

--- resources/views/front/client/request/create.blade.php
+++ resources/views/front/client/request/create.blade.php
@@ -241,20 +241,16 @@
                                     </div><!--/.col-*-->
                                 </div><!--/.row-->
                                 <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
+                                    <div class="input-field col s6 ">
                                             <select name="from" id="from">
                                             </select>
                                             <label for="from"> Choose starting time </label>
-                                        </div><!-- /.form-group -->
                                     </div>
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
+                                    <div class="input-field col s6 ">
                                             <select name="to" id="to">
                                             </select>
                                             <label for="to"> Choose ending time </label>
-                                        </div><!-- /.form-group -->
-                                    </div><!-- /.col-* -->
+                                    </div>
                                 </div><!-- /.row -->
 
 
