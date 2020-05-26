22ce866029df5cea865c915a71d546b1f0ca9fe4
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-27 02:26:35 +0300

    no message

--- app/Http/Controllers/FRONT/Employee/ProfileController.php
+++ app/Http/Controllers/FRONT/Employee/ProfileController.php
@@ -137,8 +137,6 @@ class ProfileController extends Controller
     }
     public function uploadAny($file, $folder, $ext = 'png')
     {
-        $file = base64_decode($file);
-
         $file_name = Str::random(25) . '.' . $ext; //generating unique file name;
         if (!Storage::disk('public')->exists($folder)) {
             Storage::disk('public')->makeDirectory($folder);
