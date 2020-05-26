e771e567affd05a40385ae9e849e8a651de8236b
mohammad <itani0369-@hotmail.com>
2020-05-27 01:37:04 +0300

    hadde

--- resources/views/cms/reports/show.blade.php
+++ resources/views/cms/reports/show.blade.php
@@ -1,46 +1,15 @@
 @extends('cms.layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="main-card mb-3 card">
-                <div class="card-body"><h5 class="card-title">Pending Requests</h5>
-
-                    <table class="mb-0 table">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Client</th>
-                            <th>Service</th>
-                            <th>Subject</th>
-                            <th>
-                                Date
-                            </th>
-                            <th>Open Report</th>
-
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($requests as $request)
-                            <tr id="row-{{$request->id}}">
-                                <th scope="row">{{$loop->index +1 }}</th>
-                                <td> {{ $request->client['name'] }}</td>
-                                <td>{{$request->handyman['name']}}</td>
-                                <td> {{ $request->service['name'] }}</td>
-
-                                <td>{{$request->subject}}</td>
-                                <td>{{$request->date}}</td>
-
-
-                            </tr>
-                            <tr></tr>
-                        @endforeach
-
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Title of the document</title>
+</head>
+<body>
+<h1>PDF Example with iframe</h1>
+<iframe src="http://www.orimi.com/pdf-test.pdf" width="100%" height="500px">
+</iframe>
+</body>
+</html>
 @endsection
