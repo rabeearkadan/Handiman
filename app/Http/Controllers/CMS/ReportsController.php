51af61b39aed6aa9bb5a18e14eea8e81194bd5ab
mohammad <itani0369-@hotmail.com>
2020-05-27 01:31:33 +0300

    .

--- app/Http/Controllers/CMS/ReportsController.php
+++ app/Http/Controllers/CMS/ReportsController.php
@@ -4,15 +4,30 @@ namespace App\Http\Controllers\CMS;
 
 use App\Http\Controllers\Controller;
 use App\Models\RequestService;
+use App\Models\Service;
+use App\User;
 
 class ReportsController extends Controller
 {
 
     public function index()
     {
-        $reports = RequestService::query()->where('report', '!=', null);
+        $_requests = RequestService::query()->where('isdone', true);
 
-        return view('cms.reports.index', compact('reports'));
+
+        $requests = $_requests->map(function ($item) {
+            $item->client = User::query()->find($item->client_ids[0])->simplifiedArray();
+            $item->handyman = User::query()->find($item->employee_ids[0])->simplifiedArray();
+            $item->service = Service::query()->find($item->service_id)->ServiceArray();
+            return $item;
+        });
+        return view('cms.reports.index', compact('requests'));
+    }
+
+    public function show($id)
+    {
+        $request = RequestService::query()->find($id);
+        return view('cms.reports.show', compact('request'));
     }
 
 }
