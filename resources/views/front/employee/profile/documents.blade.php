b1c67ee3d27334ac927f58e5499869955fbbd813
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-27 01:15:25 +0300

    no message

--- resources/views/front/employee/profile/documents.blade.php
+++ resources/views/front/employee/profile/documents.blade.php
@@ -5,14 +5,14 @@
     <div class="page-title">
         <h1>Documents</h1>
     </div><!-- /.page-title -->
-
     <div class="background-white p20 mb30">
         <form method="post" action="#" enctype="multipart/form-data">
+            @csrf
+            @method('put')
         <h3 class="page-title">
             Resumé
             <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
         </h3>
-
         <div class="row">
             <div class="col-sm-12">
                 <div class="file-field input-field">
@@ -28,7 +28,29 @@
         </div><!-- /.row -->
         </form>
     </div>
-
+    <div class="background-white p20 mb30">
+        <form method="post" action="#" enctype="multipart/form-data">
+            @csrf
+            @method('put')
+            <h3 class="page-title">
+                Criminal Record
+                <a href="#" class="btn btn-primary btn-xs pull-right">Save</a>
+            </h3>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="file-field input-field">
+                        <div class="btn" style="position: static">
+                            <span>File</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" name="cv" type="text" accept="application/pdf">
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.row -->
+        </form>
+    </div>
 @endsection
 @push('js')
 
