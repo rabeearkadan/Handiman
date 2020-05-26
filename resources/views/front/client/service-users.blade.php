52df98c798e874cf7310f174772eb0ae4d8971f9
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-26 04:51:27 +0300

    no message

--- resources/views/front/client/service-users.blade.php
+++ resources/views/front/client/service-users.blade.php
@@ -68,14 +68,12 @@
 
                     <hr>
 
-{{--                    <div class="row">--}}
-{{--                        <div class="col-sm-8">--}}
-{{--                            <div class="filter-actions">--}}
-{{--                                <a href="#"><i class="fa fa-close"></i> Reset Filter</a>--}}
-{{--                                <a href="#"><i class="fa fa-save"></i> Save Search</a>--}}
-{{--                            </div><!-- /.filter-actions -->--}}
-{{--                        </div><!-- /.col-* -->--}}
-<div class="row">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <div class="filter-actions">
+                                <a href="#"><i class="fa fa-close"></i> Reset Filter</a>
+                            </div><!-- /.filter-actions -->
+                        </div><!-- /.col-* -->
                         <div class="col-sm-4">
                             <button type="submit" class="btn btn-primary">Redefine Search Result</button>
                         </div><!-- /.col-* -->
