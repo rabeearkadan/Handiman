cc583ca38e1cb010fbbd918ed2b37e73e424694c
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-29 04:25:45 +0300

    no message

--- resources/views/front/client/home.blade.php
+++ resources/views/front/client/home.blade.php
@@ -51,11 +51,11 @@
                                             </div><!-- /.post-meta-categories -->
                                             <div class="post-meta-comments">
                                                 <i class="fa fa-comments"></i>
-                                                <a href="">3 comments</a>
+{{--                                                <a href="">3 comments</a>--}}
                                             </div><!-- /.post-meta-comments -->
-                                            <div class="post-meta-more">
-                                                <a href="">Read More <i class="fa fa-chevron-right"></i></a>
-                                            </div><!-- /.post-meta-more -->
+{{--                                            <div class="post-meta-more">--}}
+{{--                                                <a href="">Read More <i class="fa fa-chevron-right"></i></a>--}}
+{{--                                            </div><!-- /.post-meta-more -->--}}
                                         </div><!-- /.post-meta -->
                                     </div><!-- /.post -->
                                     @endforeach
