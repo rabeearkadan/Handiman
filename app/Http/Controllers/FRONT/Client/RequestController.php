c6ec7a8b02bd876989aa37a7fe47cb94a8932ae2
ahmaddorra <ahmadeldorra1@gmail.com>
2020-05-29 04:34:36 +0300

    no message

--- app/Http/Controllers/FRONT/Client/RequestController.php
+++ app/Http/Controllers/FRONT/Client/RequestController.php
@@ -191,7 +191,7 @@ class RequestController extends Controller
         }
         else {
                 $handyman = User::query()->find($req->input('employee_id'));
-                $requestHandyman->date = Carbon::createFromFormat('d-m-Y', $req->input('date'), $requestHandyman->timezone);
+                $requestHandyman->date = Carbon::createFromFormat('d/m/Y', $req->input('date'), $requestHandyman->timezone);
                 if ($req->has('from'))
                     $requestHandyman->from = $req->input('from');
                 if ($req->has('to')) {
