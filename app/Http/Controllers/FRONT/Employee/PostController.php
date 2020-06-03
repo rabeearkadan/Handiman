694a1ff0bc0b33ac31d2c8ec4e8250578fb96287
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-03 23:32:55 +0300

    no message

--- app/Http/Controllers/FRONT/Employee/PostController.php
+++ app/Http/Controllers/FRONT/Employee/PostController.php
@@ -80,7 +80,6 @@ class PostController extends Controller
                 array_push($images, $element);
             }
         }
-        dd($requestImages,$images);
         $post->title = $request->title;
         $post->body = $request->body;
         $post->images = $images;
