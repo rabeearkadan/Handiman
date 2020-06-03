dfe0b5cfa5c693ef51955ba877af479f1c36e479
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-03 22:30:08 +0300

    no message

--- app/Http/Controllers/FRONT/Employee/PostController.php
+++ app/Http/Controllers/FRONT/Employee/PostController.php
@@ -37,6 +37,7 @@ class PostController extends Controller
             $slideIndex[$index] = 1 ;
             $slideId[$index] = "mySlide".$index;
         }
+        dd($postCount,$slideId,$slideIndex);
         return view('front.employee.post.index', compact(['user', 'services', 'slideIndex', 'slideId','postCount']));
     }
 
