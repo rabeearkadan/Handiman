841bf0107e0f944992898aa17adc6e18f177ee20
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-03 22:39:50 +0300

    no message

--- resources/views/front/employee/post/index.blade.php
+++ resources/views/front/employee/post/index.blade.php
@@ -171,24 +171,24 @@
 {{--        }--}}
 {{--    </script>--}}
     <script>
-        {{--var slideIndex = @json($slideIndex);--}}
-        {{--var slideId = @json($slideId);--}}
-        {{--@for($index=0;$index<$postCount;$index)--}}
-        {{--showSlides(1, {{$index}});--}}
-        {{--@endfor--}}
-        {{--function plusSlides(n, no) {--}}
-        {{--    showSlides(slideIndex[no] += n, no);--}}
-        {{--}--}}
+        var slideIndex = @json($slideIndex);
+        var slideId = @json($slideId);
+        @for($index=0;$index<$postCount;$index)
+        showSlides(1, {{$index}});
+        @endfor
+        function plusSlides(n, no) {
+            showSlides(slideIndex[no] += n, no);
+        }
 
-        {{--function showSlides(n, no) {--}}
-        {{--    var i;--}}
-        {{--    var x = document.getElementsByClassName(slideId[no]);--}}
-        {{--    if (n > x.length) {slideIndex[no] = 1}--}}
-        {{--    if (n < 1) {slideIndex[no] = x.length}--}}
-        {{--    for (i = 0; i < x.length; i++) {--}}
-        {{--        x[i].style.display = "none";--}}
-        {{--    }--}}
-        {{--    x[slideIndex[no]-1].style.display = "block";--}}
-        {{--}--}}
+        function showSlides(n, no) {
+            var i;
+            var x = document.getElementsByClassName(slideId[no]);
+            if (n > x.length) {slideIndex[no] = 1}
+            if (n < 1) {slideIndex[no] = x.length}
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            x[slideIndex[no]-1].style.display = "block";
+        }
     </script>
 @endpush
