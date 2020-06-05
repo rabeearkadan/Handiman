fc63e37b78ebcac7a7cde29a00cdb9e9a05de6a1
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-05 06:55:02 +0300

    no message

--- resources/views/front/client/employee-profile.blade.php
+++ resources/views/front/client/employee-profile.blade.php
@@ -261,146 +261,120 @@
                     @endisset
                     </div>
                 @endforeach
-
             </div><!-- /.list -->
         </div><!-- /#reviews-list -->
-
-        <div class="l-row l-row--margin-top medium-hide">
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-            <!---->
-
-        </div>
     </section>
 
 
-
+    
     <div id="modal-container">
-
     </div>
 
 
 
 
 
-    <section class="experience-section section">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="heading">
-                        <h3><b>Work Experience</b></h3>
-                        <h6 class="font-lite-black"><b>PREVIOUS JOBS</b></h6>
-                    </div>
-                </div><!-- col-sm-4 -->
-                <div class="col-sm-8">
+{{--    <section class="experience-section section">--}}
+{{--        <div class="container">--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-sm-4">--}}
+{{--                    <div class="heading">--}}
+{{--                        <h3><b>Work Experience</b></h3>--}}
+{{--                        <h6 class="font-lite-black"><b>PREVIOUS JOBS</b></h6>--}}
+{{--                    </div>--}}
+{{--                </div><!-- col-sm-4 -->--}}
+{{--                <div class="col-sm-8">--}}
 
-                    <div class="experience margin-b-50">
-                        <h4><b>JUNIOR PROJECT MANAGER</b></h4>
-                        <h5 class="font-yellow"><b>DESIGN STUDIO</b></h5>
-                        <h6 class="margin-t-10">MARCH 2015 - PRESENT</h6>
-                        <p class="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus
-                            aliquam neque sit amet sodales.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,
-                            quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                            eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-                        <ul class="list margin-b-30">
-                            <li>Duis non volutpat arcu, eu mollis tellus.</li>
-                            <li>Quis consequat nulla effi citur at.</li>
-                            <li>Sed finibus aliquam neque sit.</li>
-                        </ul>
-                    </div><!-- experience -->
+{{--                    <div class="experience margin-b-50">--}}
+{{--                        <h4><b>JUNIOR PROJECT MANAGER</b></h4>--}}
+{{--                        <h5 class="font-yellow"><b>DESIGN STUDIO</b></h5>--}}
+{{--                        <h6 class="margin-t-10">MARCH 2015 - PRESENT</h6>--}}
+{{--                        <p class="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus--}}
+{{--                            aliquam neque sit amet sodales.--}}
+{{--                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,--}}
+{{--                            quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,--}}
+{{--                            eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>--}}
+{{--                        <ul class="list margin-b-30">--}}
+{{--                            <li>Duis non volutpat arcu, eu mollis tellus.</li>--}}
+{{--                            <li>Quis consequat nulla effi citur at.</li>--}}
+{{--                            <li>Sed finibus aliquam neque sit.</li>--}}
+{{--                        </ul>--}}
+{{--                    </div><!-- experience -->--}}
 
-                    <div class="experience margin-b-50">
-                        <h4><b>WEB MASTER/WEB DEVELOPER</b></h4>
-                        <h5 class="font-yellow"><b>DESIGN & WEB STUDIO</b></h5>
-                        <h6 class="margin-t-10">APRIL 2014 - FEBRUARY 2015</h6>
-                        <p class="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus
-                            aliquam neque sit amet sodales.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,
-                            quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                            eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-                        <ul class="list margin-b-30">
-                            <li>Duis non volutpat arcu, eu mollis tellus.</li>
-                            <li>Quis consequat nulla effi citur at.</li>
-                            <li>Sed finibus aliquam neque sit.</li>
-                        </ul>
-                    </div><!-- experience -->
+{{--                    <div class="experience margin-b-50">--}}
+{{--                        <h4><b>WEB MASTER/WEB DEVELOPER</b></h4>--}}
+{{--                        <h5 class="font-yellow"><b>DESIGN & WEB STUDIO</b></h5>--}}
+{{--                        <h6 class="margin-t-10">APRIL 2014 - FEBRUARY 2015</h6>--}}
+{{--                        <p class="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus--}}
+{{--                            aliquam neque sit amet sodales.--}}
+{{--                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,--}}
+{{--                            quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,--}}
+{{--                            eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>--}}
+{{--                        <ul class="list margin-b-30">--}}
+{{--                            <li>Duis non volutpat arcu, eu mollis tellus.</li>--}}
+{{--                            <li>Quis consequat nulla effi citur at.</li>--}}
+{{--                            <li>Sed finibus aliquam neque sit.</li>--}}
+{{--                        </ul>--}}
+{{--                    </div><!-- experience -->--}}
 
-                </div><!-- col-sm-8 -->
-            </div><!-- row -->
-        </div><!-- container -->
+{{--                </div><!-- col-sm-8 -->--}}
+{{--            </div><!-- row -->--}}
+{{--        </div><!-- container -->--}}
 
-    </section><!-- experience-section -->
+{{--    </section><!-- experience-section -->--}}
 
-    <section class="education-section section">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="heading">
-                        <h3><b>Education</b></h3>
-                        <h6 class="font-lite-black"><b>ACADEMIC CAREER</b></h6>
-                    </div>
-                </div><!-- col-sm-4 -->
-                <div class="col-sm-8">
-                    <div class="education-wrapper">
-                        <div class="education margin-b-50">
-                            <h4><b>MASTER DEGREE IN SCIENCE</b></h4>
-                            <h5 class="font-yellow"><b>UCLA - SCIENCE AND ENGINEERING</b></h5>
-                            <h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2010(2 YEARS)</h6>
-                            <p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque
-                                sit amet sodales.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que
-                                velit,
-                                quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                                eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-                        </div><!-- education -->
+{{--    <section class="education-section section">--}}
+{{--        <div class="container">--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-sm-4">--}}
+{{--                    <div class="heading">--}}
+{{--                        <h3><b>Education</b></h3>--}}
+{{--                        <h6 class="font-lite-black"><b>ACADEMIC CAREER</b></h6>--}}
+{{--                    </div>--}}
+{{--                </div><!-- col-sm-4 -->--}}
+{{--                <div class="col-sm-8">--}}
+{{--                    <div class="education-wrapper">--}}
+{{--                        <div class="education margin-b-50">--}}
+{{--                            <h4><b>MASTER DEGREE IN SCIENCE</b></h4>--}}
+{{--                            <h5 class="font-yellow"><b>UCLA - SCIENCE AND ENGINEERING</b></h5>--}}
+{{--                            <h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2010(2 YEARS)</h6>--}}
+{{--                            <p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque--}}
+{{--                                sit amet sodales.--}}
+{{--                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que--}}
+{{--                                velit,--}}
+{{--                                quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,--}}
+{{--                                eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>--}}
+{{--                        </div><!-- education -->--}}
 
-                        <div class="education margin-b-50">
-                            <h4><b>COURSE ON COMPUTER SCIENCE</b></h4>
-                            <h5 class="font-yellow"><b>NEW YORK PUBLIC UNIVERSITY</b></h5>
-                            <h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2009(6 MONTHS)</h6>
-                            <p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque
-                                sit amet sodales.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que
-                                velit,
-                                quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                                eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-                        </div><!-- education -->
+{{--                        <div class="education margin-b-50">--}}
+{{--                            <h4><b>COURSE ON COMPUTER SCIENCE</b></h4>--}}
+{{--                            <h5 class="font-yellow"><b>NEW YORK PUBLIC UNIVERSITY</b></h5>--}}
+{{--                            <h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2009(6 MONTHS)</h6>--}}
+{{--                            <p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque--}}
+{{--                                sit amet sodales.--}}
+{{--                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que--}}
+{{--                                velit,--}}
+{{--                                quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,--}}
+{{--                                eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>--}}
+{{--                        </div><!-- education -->--}}
 
-                        <div class="education margin-b-50">
-                            <h4><b>GRADUATED VALEDICTERIAN</b></h4>
-                            <h5 class="font-yellow"><b>PUBLIC COLLEGE</b></h5>
-                            <h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2008(4 YEARS)</h6>
-                            <p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque
-                                sit amet sodales.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que
-                                velit,
-                                quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                                eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-                        </div><!-- education -->
-                    </div><!-- education-wrapper -->
-                </div><!-- col-sm-8 -->
-            </div><!-- row -->
-        </div><!-- container -->
+{{--                        <div class="education margin-b-50">--}}
+{{--                            <h4><b>GRADUATED VALEDICTERIAN</b></h4>--}}
+{{--                            <h5 class="font-yellow"><b>PUBLIC COLLEGE</b></h5>--}}
+{{--                            <h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2008(4 YEARS)</h6>--}}
+{{--                            <p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque--}}
+{{--                                sit amet sodales.--}}
+{{--                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que--}}
+{{--                                velit,--}}
+{{--                                quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,--}}
+{{--                                eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>--}}
+{{--                        </div><!-- education -->--}}
+{{--                    </div><!-- education-wrapper -->--}}
+{{--                </div><!-- col-sm-8 -->--}}
+{{--            </div><!-- row -->--}}
+{{--        </div><!-- container -->--}}
 
-    </section><!-- about-section -->
+{{--    </section><!-- about-section -->--}}
 
     <section class="counter-section" id="counter">
         <div class="container">
