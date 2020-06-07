1253837c0e35c2c66232d17364a540ba82fced0e
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-07 16:34:33 +0300

    no message

--- app/Http/Controllers/FRONT/Client/RequestController.php
+++ app/Http/Controllers/FRONT/Client/RequestController.php
@@ -49,6 +49,7 @@ class RequestController extends Controller
             $item->employee = User::find($item->employee_ids[0]);
             return $item;
         });
+        dd($approvedRequests,$pendingRequests);
         return view('front.client.request.index', compact(['pendingRequests', 'approvedRequests']));
     }
 
