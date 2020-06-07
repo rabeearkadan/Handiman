3643e95a8c7722ac8a50302cc1f78cad14079b77
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-07 22:33:19 +0300

    no message

--- resources/views/front/client/request/index.blade.php
+++ resources/views/front/client/request/index.blade.php
@@ -55,6 +55,9 @@
                                         <a class="btn-floating btn-large blue" style="float:right">
                                             <i class="large material-icons">chat</i>
                                         </a>
+                                        <a class="btn-floating btn-large red" style="float:right">
+                                            <i class="large material-icons">cancel</i>
+                                        </a>
                                     @else
                                         <img src="/public/images/employee/profile.png" alt="" class="circle">
                                         <span class="title">Searching for employee</span>
