f4d496467646a22e60b6c04933ecb6e7845f8ea1
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-07 17:00:38 +0300

    no message

--- resources/views/front/client/request/index.blade.php
+++ resources/views/front/client/request/index.blade.php
@@ -109,7 +109,6 @@
         </div>
         @if($pendingRequests->count() == 0)
             <div class="container" style=" background-repeat: no-repeat; background-position: center;height: -webkit-fill-available;background-image:url('/public/images/client/pending-empty.png');">
-                <p>Wow, such empty</p>
             </div>
         @endif
     </div>
@@ -204,7 +203,6 @@
         </div>
         @if($approvedRequests->count() == 0)
             <div class="container" style=" background-repeat: no-repeat;background-position: center;height: -webkit-fill-available;background-image:url('/public/images/client/approved-empty.png');">
-                <p>Wow, such empty</p>
             </div>
         @endif
     </div>
