2ad968ea9531e257a63bf646580d50cbdd98eb8e
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-08 03:39:25 +0300

    no message

--- resources/views/front/client/profile/edit-profile.blade.php
+++ resources/views/front/client/profile/edit-profile.blade.php
@@ -108,11 +108,11 @@
                 <label>Gender</label>
                 <p>
                     <label>
-                        <input class="with-gap" name="gender" type="radio" @if($user->gender=='male') checked @endif />
+                        <input class="with-gap" name="gender" type="radio" value="male" @if($user->gender=='male') checked @endif />
                         <span>male</span>
                     </label>
                     <label>
-                        <input class="with-gap" name="gender" type="radio" @if($user->gender=='female') checked @endif />
+                        <input class="with-gap" name="gender" type="radio" value="female" @if($user->gender=='female') checked @endif />
                         <span>female</span>
                     </label>
                 </p>
