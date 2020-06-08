a06897913a2499bd8226444cd8b049d1ca5eaca4
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-08 04:00:58 +0300

    no message

--- app/Http/Controllers/FRONT/Client/HomeController.php
+++ app/Http/Controllers/FRONT/Client/HomeController.php
@@ -45,7 +45,6 @@ class HomeController extends Controller
     {
         $user = Auth::user();
         $service = Service::query()->find($id);
-        dd($service,$id);
         $client_address = null;
         foreach ($user->client_addresses as $address) {
             if ($address['_id'] == $request->address) {
