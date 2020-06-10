f1bce151e04c2264131998263b482467a4c619fc
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-10 12:01:33 +0300

    no message

--- resources/views/layouts/employee/app.blade.php
+++ resources/views/layouts/employee/app.blade.php
@@ -11,7 +11,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     @endif
     @if(request()->route()->getName() == "employee.post.create" || request()->route()->getName() == "employee.home" || request()->route()->getName() == "employee.calendar.show")
-        <script src="{{ asset('js/bootstrap.js') }}" defer></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     @endif
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
