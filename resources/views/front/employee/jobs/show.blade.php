d8a188f431754f5c28b96dc18c90e65f113ae57f
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-10 04:17:25 +0300

    no message

--- resources/views/front/employee/jobs/show.blade.php
+++ resources/views/front/employee/jobs/show.blade.php
@@ -10,9 +10,26 @@
                 Request Details
             </div>
             <div class="collapsible-body">
-                <span>Request client.</span>
-                <span>Request.subject</span>
-                <span>Request.desc</span>
+                <span>Subject.</span>
+                <span>{{$job->subject}}</span>
+                <br>
+                <span>Description</span>
+                <span>{{$job->description}}</span>
+            </div>
+        </li>
+        <li>
+            <div class="collapsible-header">
+                <i class="material-icons">details</i>
+                Client Details
+            </div>
+            <div class="collapsible-body">
+                <span>{{$client->name}}</span>
+                <br>
+                <span>{{$client->phone}}</span>
+                <br>
+                <span></span>
+                <br>
+                <span></span>
             </div>
         </li>
         <li>
@@ -21,40 +38,55 @@
                 Service
             </div>
             <div class="collapsible-body">
-                <span>Service type</span>
+                <span>{{$service->name}}</span>
 
             </div>
         </li>
         <li>
             <div class="collapsible-header">
                 <i class="material-icons">event</i>
-                Date & Time
+                Date &amp; Time
             </div>
             <div class="collapsible-body">
-                <span>Date</span>
-                <span>Time</span>
+                <span>{{$job->date}}  {{$job->from}} -> {{$job->to}}</span>
             </div>
         </li>
         <li>
             <div class="collapsible-header">
                 <i class="material-icons">place</i>
-                Second
+                Address
             </div>
             <div class="collapsible-body">
-                <span>Lorem ipsum dolor sit amet.</span>
+                <h5>Name</h5>
+                <h5>state</h5>
+                <h5>city</h5>
+                <h5>Street: </h5>
+                <span>{{$job->client_address['street']}}</span><br>
+                <h5>Property Type: </h5>
+                <span>{{$job->client_address['property_type']}}</span><br>
+                <h5>Building: </h5>
+                <span>{{$job->client_address['builiding']}}</span><br>
+                <h5>Floor: </h5>
+                <span>{{$job->client_address['floor']}}</span><br>
+                <h5>zip: </h5>
+                <span>{{$job->client_address['zip']}}</span><br>
             </div>
         </li>
         <li>
             <div class="collapsible-header"><i class="material-icons">images</i>
                 Images</div>
             <div class="collapsible-body">
-{{--                <img class="materialboxed"  width="250" src="{{config('image.path').$image}}">--}}
+                @foreach($job->images as $image)
+                <img class="materialboxed"  width="350" src="{{config('image.path').$image}}">
+                    @endforeach
                 </div>
         </li>
         <li>
             <div class="collapsible-header"><i class="material-icons">payment</i>
-                bill</div>
+                Bill
+            </div>
             <div class="collapsible-body">
+                @if($job->ispaid == false)
                 <form method="post" action="{{route('employee.calendar.add.receipt',$job->id)}}" enctype="multipart/form-data">
                     @csrf
                 <span>Items</span>
@@ -113,8 +145,9 @@
                     <button class="btn waves-effect waves-light right" type="submit" name="action" style="line-height: 3.5px;font-size: small;height: 27px;margin-right: 85px;">Submit
                         <i class="material-icons right">send</i>
                     </button>
-
                 </form>
+                    @else
+                    @endif
             </div>
         </li>
     </ul>
