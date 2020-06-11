123426f4a7b816eb20083b2ec404037dabc98d2c
mohammad <itani0369-@hotmail.com>
2020-06-11 15:59:25 +0300

    Update sidebar.blade.php

--- resources/views/cms/layouts/sidebar.blade.php
+++ resources/views/cms/layouts/sidebar.blade.php
@@ -1,7 +1,8 @@
 <div class="app-sidebar sidebar-shadow">
     <div class="app-header__logo">
-        <div class="logo-src">
-        <img src="../../../../public/img/logo.png" alt="" title=""/></div>
+
+{{--        <a href="#hero"><img src="../../../../public/img/logo.png" alt="" title=""/></a>--}}
+
         <div class="header__pane ml-auto">
             <div>
                 <button type="button" class="hamburger close-sidebar-btn hamburger--elastic"
