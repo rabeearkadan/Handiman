3a968c5d5056c227060c4bdea6b0d22faf529500
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-11 17:52:20 +0300

    no message

--- resources/views/front/client/invoice/show.blade.php
+++ resources/views/front/client/invoice/show.blade.php
@@ -238,16 +238,22 @@
                                         <dd>{{$request->total}}</dd>
                                     </dl>
                                     @if($request->ispaid == false)
-                                        <form id="payment-form">
-                                            <div id="card-element">
-                                                <!-- Elements will create input elements here -->
+                                        <form action="#" method="post" id="payment-form">
+                                            @csrf
+                                            <div class="form-group">
+                                                <div class="card-header">
+                                                    <label for="card-element">
+                                                        Enter your credit card information
+                                                    </label>
+                                                </div>
+                                                <div class="card-body">
+                                                    <div id="card-element">
+                                                        <!-- A Stripe Element will be inserted here. -->
+                                                    </div>
+                                                    <!-- Used to display form errors. -->
+                                                    <div id="card-errors" role="alert"></div>
+                                                </div>
                                             </div>
-
-                                            <!-- We'll put the error messages in this element -->
-                                            <div id="card-errors" role="alert"></div>
-
-                                            <button id="submit">Pay</button>
-                                        </form>
                                     @else
                                        <p>paid</p>
                                     @endif
@@ -263,64 +269,79 @@
 @push('js')
     <script src="https://js.stripe.com/v3/"></script>
     <script>
-        var style = {
-            base: {
-                color: "#32325d",
-            }
-        };
-        // Set your publishable key: remember to change this to your live publishable key in production
-        // See your keys here: https://dashboard.stripe.com/account/apikeys
-        var stripe = Stripe('pk_test_Yzk4eIQ2VOEGQFZ70vFBuQur00xW3XqfFv');
+        // Create a Stripe client.
+        var stripe = Stripe('pk_test_odvgt7E927FAl1dxn4Pc4rMz003E8gLLw0');
+
+        // Create an instance of Elements.
         var elements = stripe.elements();
 
-        var card = elements.create("card", { style: style });
-        card.mount("#card-element");
+        // Custom styling can be passed to options when creating an Element.
+        // (Note that this demo uses a wider set of styles than the guide below.)
+        var style = {
+            base: {
+                color: '#32325d',
+                lineHeight: '18px',
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSmoothing: 'antialiased',
+                fontSize: '16px',
+                '::placeholder': {
+                    color: '#aab7c4'
+                }
+            },
+            invalid: {
+                color: '#fa755a',
+                iconColor: '#fa755a'
+            }
+        };
 
-        card.on('change', ({error}) => {
-            const displayError = document.getElementById('card-errors');
-            if (error) {
-                displayError.textContent = error.message;
+        // Create an instance of the card Element.
+        var card = elements.create('card', {style: style});
+
+        // Add an instance of the card Element into the `card-element` <div>.
+        card.mount('#card-element');
+
+        // Handle real-time validation errors from the card Element.
+        card.addEventListener('change', function(event) {
+            var displayError = document.getElementById('card-errors');
+            if (event.error) {
+                displayError.textContent = event.error.message;
             } else {
                 displayError.textContent = '';
             }
         });
 
+        // Handle form submission.
         var form = document.getElementById('payment-form');
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-        form.addEventListener('submit', function(ev) {
-            ev.preventDefault();
-         var secrent_token=   Stripe.createToken({
-                number: $('.card-number').val(),
-                cvc: $('.card-cvc').val(),
-                exp_month: $('.card-expiry-month').val(),
-                exp_year: $('.card-expiry-year').val()
-            });
-            stripe.confirmCardPayment(secrent_token, {
-                payment_method: {
-                    card: card,
-                    billing_details: {
-                        name: 'Jenny Rosen'
-                    }
-                }
-            }).then(function(result) {
+            stripe.createToken(card).then(function(result) {
                 if (result.error) {
-                    // Show error to your customer (e.g., insufficient funds)
-                    console.log(result.error.message);
+                    // Inform the user if there was an error.
+                    var errorElement = document.getElementById('card-errors');
+                    errorElement.textContent = result.error.message;
                 } else {
-                    // The payment has been processed!
-                    if (result.paymentIntent.status === 'succeeded') {
-                        // Show a success message to your customer
-                        // There's a risk of the customer closing the window before callback
-                        // execution. Set up a webhook or plugin to listen for the
-                        // payment_intent.succeeded event that handles any business critical
-                        // post-payment actions.
-                        console.log(result);
-                        console.log(result.paymentIntent.token());
-                    }
+                    // Send the token to your server.
+                    stripeTokenHandler(result.token);
                 }
             });
         });
 
+        // Submit the form with the token ID.
+        function stripeTokenHandler(token) {
+            // Insert the token ID into the form so it gets submitted to the server
+            var form = document.getElementById('payment-form');
+            var hiddenInput = document.createElement('input');
+            hiddenInput.setAttribute('type', 'hidden');
+            hiddenInput.setAttribute('name', 'stripeToken');
+            hiddenInput.setAttribute('value', token.id);
+            form.appendChild(hiddenInput);
+
+            // Submit the form
+            form.submit();
+        }
+    </script>
+<script>
         document.addEventListener('DOMContentLoaded', function () {
             var elems = document.querySelectorAll('.collapsible');
             var instances = M.Collapsible.init(elems);
