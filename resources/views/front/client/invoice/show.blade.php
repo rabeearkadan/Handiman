db82c66c902fefa71c96486c7b24bfd3bcf18806
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-11 15:03:42 +0300

    no message

--- resources/views/front/client/invoice/show.blade.php
+++ resources/views/front/client/invoice/show.blade.php
@@ -4,6 +4,8 @@
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <link href="{{asset('css/client/invoice.css')}}" rel="stylesheet">
     <link href="{{asset('css/client/colorbox.css')}}" rel="stylesheet">
+
+    <script src="https://js.stripe.com/v3/"></script>
     <style>
         .row {
             display: flex;
@@ -238,9 +240,16 @@
                                         <dd>{{$request->total}}</dd>
                                     </dl>
                                     @if($request->ispaid == false)
-                                        <a href="" class="btn waves-effect waves-light left" >
-                                            Pay
-                                        </a>
+                                        <form id="payment-form">
+                                            <div id="card-element">
+                                                <!-- Elements will create input elements here -->
+                                            </div>
+
+                                            <!-- We'll put the error messages in this element -->
+                                            <div id="card-errors" role="alert"></div>
+
+                                            <button id="submit">Pay</button>
+                                        </form>
                                     @else
                                        <p>paid</p>
                                     @endif
@@ -255,6 +264,55 @@
 @endsection
 @push('js')
     <script>
+        var style = {
+            base: {
+                color: "#32325d",
+            }
+        };
+        // Set your publishable key: remember to change this to your live publishable key in production
+        // See your keys here: https://dashboard.stripe.com/account/apikeys
+        var stripe = Stripe('pk_test_Yzk4eIQ2VOEGQFZ70vFBuQur00xW3XqfFv');
+        var elements = stripe.elements();
+
+        var card = elements.create("card", { style: style });
+        card.mount("#card-element");
+
+        card.on('change', ({error}) => {
+            const displayError = document.getElementById('card-errors');
+            if (error) {
+                displayError.textContent = error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        var form = document.getElementById('payment-form');
+
+        form.addEventListener('submit', function(ev) {
+            ev.preventDefault();
+            stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: 'Jenny Rosen'
+                    }
+                }
+            }).then(function(result) {
+                if (result.error) {
+                    // Show error to your customer (e.g., insufficient funds)
+                    console.log(result.error.message);
+                } else {
+                    // The payment has been processed!
+                    if (result.paymentIntent.status === 'succeeded') {
+                        // Show a success message to your customer
+                        // There's a risk of the customer closing the window before callback
+                        // execution. Set up a webhook or plugin to listen for the
+                        // payment_intent.succeeded event that handles any business critical
+                        // post-payment actions.
+                    }
+                }
+            });
+        });
 
         document.addEventListener('DOMContentLoaded', function () {
             var elems = document.querySelectorAll('.collapsible');
