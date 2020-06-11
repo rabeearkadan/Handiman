d1ffa6326cf8d93261c124c1cb66065c638683e6
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-11 22:35:55 +0300

    no message

--- app/Http/Controllers/FRONT/Client/InvoiceController.php
+++ app/Http/Controllers/FRONT/Client/InvoiceController.php
@@ -67,7 +67,7 @@ class InvoiceController extends Controller
         $user = Auth::user();
         Stripe::setApiKey('sk_test_rPUYuVgziB8APOOSyd9q4zgT00rtI4Hhat');
         $job = RequestService::query()->find($id);
-        $employee = User::query()->find($request->employee_ids[0]);
+        $employee = User::query()->find($job->employee_ids[0]);
 
         $total = $job->total;
         $token = $request->stripeToken;
