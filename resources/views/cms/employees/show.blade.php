a7017c241afadaaf65b81cbc581570f2b2a48bd1
mohammad <itani0369-@hotmail.com>
2020-06-12 21:18:55 +0300

    Update show.blade.php

--- resources/views/cms/employees/show.blade.php
+++ resources/views/cms/employees/show.blade.php
@@ -129,7 +129,7 @@
 
                                 <td>
                                     <form
-                                        action="{{ route('employee.removeService', [  $employee->id,  'id2'=> $service['id']]) }}"
+                                        action="{{ route('employee.removeService', [  $employee->id,   $service['id']]) }}"
                                             method="GET"
                                     >
 
