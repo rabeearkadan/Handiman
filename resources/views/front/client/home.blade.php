fd4411f6f1554a463318a6a64e93df861cc27a7e
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-12 16:57:17 +0300

    no message

--- resources/views/front/client/home.blade.php
+++ resources/views/front/client/home.blade.php
@@ -57,7 +57,7 @@
                                                             <a href="{{route('client.user-profile',['employee_id' => $user->id])}}"> {{$user->name}} </a>
                                                         @endforeach
                                                     </div><!-- /.post-meta-author -->
-                                                    <div class="post-meta-date"> {{$post->created_at}} </div>
+                                                    <div class="post-meta-date"> {{$post->created_at->format('d/m/Y')}} </div>
                                                     <!-- /.post-meta-date -->
                                                     <div class="post-meta-categories">
                                                         <i class="fa fa-tags"></i>
