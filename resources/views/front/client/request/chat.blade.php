09663acedacfa5ad2c5e0932116109628c7e1c59
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-12 12:05:00 +0300

    no message

--- resources/views/front/client/request/chat.blade.php
+++ resources/views/front/client/request/chat.blade.php
@@ -42,8 +42,7 @@
 
                     <div class="type_msg">
                         <div class="input_msg_write">
-                            <form id="sendForm" method="post" action="javascript:void(0)">
-                            @csrf
+                            <form id="sendForm">
                             <input type="text" id="message" name="message" class="write_msg" placeholder="Type a message" />
                             <button class="msg_send_btn" type="submit"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
                             </form>
