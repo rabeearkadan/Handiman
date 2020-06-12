a142cdaf141e016d92d59a53af6364ebcd54203b
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-12 14:19:15 +0300

    no message

--- resources/views/front/employee/chat.blade.php
+++ resources/views/front/employee/chat.blade.php
@@ -51,12 +51,8 @@
 @endsection
 @push('js')
     <script>
-        const messages = document.getElementsByClassName('msg_history');
-
         function update(){
             var numberOfMessages = document.getElementsByClassName('incoming_msg').length;
-            shouldScroll = messages.scrollTop + messages.clientHeight === messages.scrollHeight;
-
             $.ajax({
                 type: 'GET',
                 url: "{{ route('client.chat.new',$request->id) }}",
@@ -74,21 +70,14 @@
                                 '<span class="time_date">' + data.messages[index]['date'] + '</span></div>' +
                                 '</div>\n' +
                                 '</div>');
+                            scroll();
                         }
                     }
                 }
             });
-            if (!shouldScroll) {
-                scrollToBottom();
-            }
             setTimeout(update, 3000);
         }
         update();
-        function scrollToBottom() {
-            messages.scrollTop = messages.scrollHeight;
-        }
-
-        scrollToBottom();
 
         $(".msg_send_btn").click(function (e) {
             e.preventDefault();
@@ -105,9 +94,14 @@
                             '<span class="time_date">' + data.date + '</span> </div>\n' +
                             '</div>');
                         $("#message").val('');
+                        scroll();
                     }
                 });
             }
         });
+        function scroll() {
+            var elem = document.getElementsByClassName('msg_history');
+            elem.scrollTop = elem.scrollHeight;
+        }
     </script>
 @endpush
