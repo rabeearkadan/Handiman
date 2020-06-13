2fde3ec5375a0f7a373b7a43770c11a5207e90d8
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-13 04:56:39 +0300

    no message

--- resources/views/front/client/service-users.blade.php
+++ resources/views/front/client/service-users.blade.php
@@ -196,7 +196,7 @@
                 onOpen: function () {
                     var instance = M.Datepicker.getInstance($('.datepicker'));
                     instance.options.minDate = new Date();
-                    instance.options.maxDate = new Date(year + 1);
+                    instance.options.maxDate = new Date(instance.options.minDate + 1);
 
                     instance.options.format = 'mm/dd/yyyy'
                 }
