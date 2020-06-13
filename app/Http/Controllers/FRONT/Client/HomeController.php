c7795cc66204f1dbb726aaa3872a39c64b77f065
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-13 15:55:57 +0300

    no message

--- app/Http/Controllers/FRONT/Client/HomeController.php
+++ app/Http/Controllers/FRONT/Client/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $posts = Post::orderBy('created_at', 'desc')->take(0)->get();
+        $posts = Post::orderBy('created_at', 'desc')->get();
         $services = Service::all();
         if($request->input('logged') == true) {
             $logged = $request->input('logged');
--- resources/views/front/client/home.blade.php
+++ resources/views/front/client/home.blade.php
@@ -30,73 +30,73 @@
                                 </div><!-- /.page-title -->
                                 <div class="chips chips-autocomplete"></div>
 
-{{--                                @if(!$posts->isEmpty())--}}
-{{--                                    <div class="posts">--}}
-{{--                                        <div id="posts-list">--}}
-{{--                                            <div class="list">--}}
-{{--                                        @foreach($posts as $post)--}}
-{{--                                            <div class="post">--}}
-{{--                                                <div class="carousel carousel-slider" id="{{$post->id}}">--}}
-{{--                                                    @foreach($post->images as $image)--}}
-{{--                                                        <img class="carousel-item" src="{{config('image.path').$image}}"--}}
-{{--                                                             alt="Post Images">--}}
-{{--                                                    @endforeach--}}
-{{--                                                </div>--}}
-{{--                                                <div class="post-content">--}}
-{{--                                                    <h2> {{$post->title}} </h2>--}}
-{{--                                                    <p> {{$post->body}} </p>--}}
-{{--                                                </div><!-- /.post-content -->--}}
-{{--                                                <div class="post-meta clearfix">--}}
-{{--                                                    <div class="post-meta-author">--}}
-{{--                                                        @foreach($post->users as $user)--}}
-{{--                                                            <a href="{{route('client.user-profile',['employee_id' => $user->id])}}">--}}
-{{--                                                                @isset($user->image)--}}
-{{--                                                                <img style="width:35px; height:35px"--}}
-{{--                                                                     class="circle responsive-img"--}}
-{{--                                                                     src="{{config('image.path').$user->image}}" alt="post images">--}}
-{{--                                                                @else--}}
-{{--                                                                    <img style="width:35px; height:35px"--}}
-{{--                                                                         class="circle responsive-img"--}}
-{{--                                                                         src="/public/images/employee/profile-image.png" alt="post images">--}}
-{{--                                                                @endisset--}}
-{{--                                                            </a>--}}
-{{--                                                            <a href="{{route('client.user-profile',['employee_id' => $user->id])}}"> {{$user->name}} </a>--}}
-{{--                                                        @endforeach--}}
-{{--                                                    </div><!-- /.post-meta-author -->--}}
-{{--                                                    <div class="post-meta-date"> {{$post->created_at->format('d/m/Y')}} </div>--}}
-{{--                                                    <!-- /.post-meta-date -->--}}
-{{--                                                    <div class="post-meta-categories">--}}
-{{--                                                        <i class="fa fa-tags"></i>--}}
+                                @if(!$posts->isEmpty())
+                                    <div class="posts">
+                                        <div id="posts-list">
+                                            <div class="list">
+                                        @foreach($posts as $post)
+                                            <div class="post">
+                                                <div class="carousel carousel-slider" id="{{$post->id}}">
+                                                    @foreach($post->images as $image)
+                                                        <img class="carousel-item" src="{{config('image.path').$image}}"
+                                                             alt="Post Images">
+                                                    @endforeach
+                                                </div>
+                                                <div class="post-content">
+                                                    <h2> {{$post->title}} </h2>
+                                                    <p> {{$post->body}} </p>
+                                                </div><!-- /.post-content -->
+                                                <div class="post-meta clearfix">
+                                                    <div class="post-meta-author">
+                                                        @foreach($post->users as $user)
+                                                            <a href="{{route('client.user-profile',['employee_id' => $user->id])}}">
+                                                                @isset($user->image)
+                                                                <img style="width:35px; height:35px"
+                                                                     class="circle responsive-img"
+                                                                     src="{{config('image.path').$user->image}}" alt="post images">
+                                                                @else
+                                                                    <img style="width:35px; height:35px"
+                                                                         class="circle responsive-img"
+                                                                         src="/public/images/employee/profile-image.png" alt="post images">
+                                                                @endisset
+                                                            </a>
+                                                            <a href="{{route('client.user-profile',['employee_id' => $user->id])}}"> {{$user->name}} </a>
+                                                        @endforeach
+                                                    </div><!-- /.post-meta-author -->
+                                                    <div class="post-meta-date"> {{$post->created_at->format('d/m/Y')}} </div>
+                                                    <!-- /.post-meta-date -->
+                                                    <div class="post-meta-categories">
+                                                        <i class="fa fa-tags"></i>
 
-{{--                                                        @foreach($post->tags as $tag)--}}
-{{--                                                            @if($loop->index !=0)--}}
-{{--                                                                ,--}}
-{{--                                                            @endif--}}
-{{--                                                            <a href="{{route('client.service', $tag->id)}}"> {{$tag->name}} </a>--}}
-{{--                                                        @endforeach--}}
-{{--                                                        <div class="categories" style="display: none">--}}
-{{--                                                            @foreach($post->tags as $tag)--}}
-{{--                                                                {{$tag->name }}--}}
-{{--                                                            @endforeach--}}
-{{--                                                        </div>--}}
-{{--                                                    </div><!-- /.post-meta-categories -->--}}
-{{--                                                    --}}{{--                                            <div class="post-meta-comments">--}}
-{{--                                                    --}}{{--                                                <i class="fa fa-comments"></i>--}}
-{{--                                                    --}}{{--                                                <a href="">3 comments</a>--}}
-{{--                                                    --}}{{--                                            </div><!-- /.post-meta-comments -->--}}
+                                                        @foreach($post->tags as $tag)
+                                                            @if($loop->index !=0)
+                                                                ,
+                                                            @endif
+                                                            <a href="{{route('client.service', $tag->id)}}"> {{$tag->name}} </a>
+                                                        @endforeach
+                                                        <div class="categories" style="display: none">
+                                                            @foreach($post->tags as $tag)
+                                                                {{$tag->name }}
+                                                            @endforeach
+                                                        </div>
+                                                    </div><!-- /.post-meta-categories -->
+                                                    {{--                                            <div class="post-meta-comments">--}}
+                                                    {{--                                                <i class="fa fa-comments"></i>--}}
+                                                    {{--                                                <a href="">3 comments</a>--}}
+                                                    {{--                                            </div><!-- /.post-meta-comments -->--}}
 {{--                                                    <div class="post-meta-more">--}}
 {{--                                                        <a href="">View <i class="fa fa-chevron-right"></i></a>--}}
 {{--                                                    </div><!-- /.post-meta-more -->--}}
-{{--                                                </div><!-- /.post-meta -->--}}
-{{--                                            </div><!-- /.post -->--}}
-{{--                                        @endforeach--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                    </div> <!-- /.posts -->--}}
-{{--                                @else--}}
+                                                </div><!-- /.post-meta -->
+                                            </div><!-- /.post -->
+                                        @endforeach
+                                            </div>
+                                        </div>
+                                    </div> <!-- /.posts -->
+                                @else
                                     <h3> No posts yet</h3>
                                     <img class="img-fluid" src="/public/images/client/no-posts.png" alt="no posts">
-{{--                                @endif--}}
+                                @endif
                             </div><!-- /.content -->
                         </div><!-- /.col-* -->
                     </div><!-- /.row -->
