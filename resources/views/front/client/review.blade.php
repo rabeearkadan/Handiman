dcf3f21e7e6f4c696b99b5258e3243a8568acb88
ahmaddorra <ahmadeldorra1@gmail.com>
2020-06-14 18:34:32 +0300

    no message

--- resources/views/front/client/review.blade.php
+++ resources/views/front/client/review.blade.php
@@ -68,7 +68,7 @@
                                                         <div class="row">
                                                             <div class="col-sm-12">
                                                                 @for($index=0;$index<$request->rating;$index++)
-                                                                    <i class="large material-icons">stars</i>
+                                                                    <i class="small material-icons">stars</i>
                                                                     @endfor
                                                             </div>
                                                         </div>
